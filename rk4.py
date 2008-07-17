33ce7c04a9fa2566e7c7201b0034cb9b3ade216d
Ian Huston <ian@ianhuston.net>
2008-07-17 16:24:18 +0000

    Swap derivs(t,y) to derivs(y,t) to make the same as scipy.odeint.

--- rk4.py
+++ rk4.py
@@ -1,7 +1,7 @@
 #
 #Runge-Kutta ODE solver
 #Author: Ian Huston
-#CVS: $Id: rk4.py,v 1.12 2008/07/09 15:58:27 ith Exp $
+#CVS: $Id: rk4.py,v 1.13 2008/07/17 16:24:18 ith Exp $
 #
 
 from __future__ import division # Get rid of integer division problems, i.e. 1/2=0
@@ -36,18 +36,18 @@ def rk4step(x, y, h, dydx, derivs):
     yt = y +hh*dydx
     
     #Second step, get new derivatives
-    dyt = derivs(xh,yt)
+    dyt = derivs(yt,xh)
     
     yt = y + hh*dyt
     
     #Third step
-    dym = derivs(xh,yt)
+    dym = derivs(yt,xh)
     
     yt = y + h*dym
     dym = dym + dyt
     
     #Fourth step
-    dyt = derivs(x+h, yt)
+    dyt = derivs(yt,x+h)
     
     #Accumulate increments with proper weights
     yout = y + h6*(dydx + dyt + 2*dym)
@@ -63,23 +63,23 @@ def rkck(y, dydx, x, h,derivs):
     ytemp = y + CKP["B21"]*h*dydx
     
     #Second step
-    ak2 = derivs(x+CKP["A2"]*h, ytemp)
+    ak2 = derivs(ytemp, x+CKP["A2"]*h)
     ytemp = y + h*(CKP["B31"]*dydx + CKP["B32"]*ak2)
     
     #Third step
-    ak3 = derivs(x + CKP["A3"]*h, ytemp)
+    ak3 = derivs(ytemp, x + CKP["A3"]*h)
     ytemp = y + h*(CKP["B41"]*dydx + CKP["B42"]*ak2 + CKP["B43"]*ak3)
     
     #Fourth step
-    ak4 = derivs(x + CKP["A4"]*h, ytemp)
+    ak4 = derivs(ytemp, x + CKP["A4"]*h)
     ytemp = y + h*(CKP["B51"]*dydx + CKP["B52"]*ak2 + CKP["B53"]*ak3 + CKP["B54"]*ak4)
     
     #Fifth step
-    ak5 = derivs(x + CKP["A5"]*h, ytemp)
+    ak5 = derivs(ytemp, x + CKP["A5"]*h)
     ytemp = y + h*(CKP["B61"]*dydx + CKP["B62"]*ak2 + CKP["B63"]*ak3 + CKP["B64"]*ak4 + CKP["B65"]*ak5)
     
     #Sixth step
-    ak6 = derivs(x + CKP["A6"]*h, ytemp)
+    ak6 = derivs(ytemp, x + CKP["A6"]*h)
     
     #Accumulate increments with proper weights.
     yout = y + h*(CKP["C1"]*dydx + CKP["C3"]*ak3 + CKP["C4"]*ak4 + CKP["C6"]*ak6)
@@ -101,7 +101,7 @@ def rkdriver_dumb(vstart, x1, x2, nstep, derivs):
     h = (x2-x1)/nstep
     
     for k in range(nstep):
-        dv = derivs(x,v)
+        dv = derivs(v, x)
         v = rk4step(x, v, h, dv, derivs)
         x = xx[k+1] = x + h
         y.append(v)
@@ -171,7 +171,7 @@ def odeint(ystart, x1, x2, h1, hmin, derivs, eps=1.0e-6, dxsav=0.0):
     xsav = x - 2.0*dxsav #always save first step
     
     for i in xrange(MAXSTP):
-        dydx = derivs(x, y)
+        dydx = derivs(y, x)
         
         yscal = abs(y) + abs(h*dydx) + TINY
         
