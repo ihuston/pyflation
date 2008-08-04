f5aafa87b6acb23ea9a8e2d2111b8a111aa56976
Ian Huston <ian@ianhuston.net>
2008-08-04 17:22:59 +0000

    Fixed nanfillstart to work with arrays.

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 """Helper functions by Ian Huston
-    $Id: helpers.py,v 1.1 2008/08/04 16:58:41 ith Exp $
+    $Id: helpers.py,v 1.2 2008/08/04 17:22:59 ith Exp $
     
     Provides helper functions for use elsewhere"""
 
@@ -7,10 +7,12 @@ from __future__ import division # Get rid of integer division problems, i.e. 1/2
 import numpy as N
 
 def nanfillstart(a, l):
-    """Return an array of length l by appending array a to end of block of NaNs."""
+    """Return an array of length l by appending array a to end of block of NaNs along axis 0."""
     if len(a) >= l:
         return a #Array already as long or longer than required
     else:
-        b = N.ones((l-len(a)))*N.NaN
-        c = N.append(b,a)
+        bshape = N.array(a.shape)
+        bshape[0] = l - bshape[0]
+        b = N.ones(bshape)*N.NaN
+        c = N.concatenate((b,a))
         return c
