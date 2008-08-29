cc7039eb387bfeaa55dfcc2e397b96602a96297a
Ian Huston <ian@ianhuston.net>
2008-08-29 17:33:58 +0000

    Started to add VODE as a solver.

--- cosmomodels.py
+++ cosmomodels.py
@@ -1,5 +1,5 @@
 """Cosmological Model simulations by Ian Huston
-    $Id: cosmomodels.py,v 1.109 2008/08/28 17:19:00 ith Exp $
+    $Id: cosmomodels.py,v 1.110 2008/08/29 17:33:58 ith Exp $
     
     Provides generic class CosmologicalModel that can be used as a base for explicit models."""
 
@@ -152,7 +152,41 @@ class CosmologicalModel(object):
                 times = N.arange(self.tstart, self.tend + self.tstep_wanted, self.tstep_wanted)
                 self.yresult = scipy_odeint(self.derivs, self.ystart, times)
                 self.tresult = times
+                
+            if self.solver == "scipy_vode":
             
+            #Use scipy solver. Need to massage derivs into right form.
+            swap_derivs = lambda t, y : self.derivs(y,t)
+                        
+            #Now split depending on whether k exists
+            if type(self.k) is N.ndarray or type(self.k) is list:
+                #Get set of times for each k
+                if type(self.tstart) is N.ndarray or type(self.tstart) is list:
+                    times = N.arange(self.tstart.min(), self.tend + self.tstep_wanted, self.tstep_wanted)
+                    startindices = [N.where(abs(ts - times)<self.eps)[0][0] for ts in self.tstart]
+                else:
+                    times = N.arange(self.tstart, self.tend + self.tstep_wanted, self.tstep_wanted)
+                    startindices = [0]
+                #Make a copy of k and ystart while we work
+                klist = N.copy(self.k)
+                yslist = N.rollaxis(N.copy(self.ystart),1,0)
+                
+                #Do calculation
+                #Compute list of ks in a row
+                
+                ylist = [scipy_odeint(self.derivs, ys, times[ts:]) for self.k, ys, ts in zip(klist,yslist,startindices)]
+                
+                ylistlengths = [len(ys) for ys in ylist]
+                ylist = [helpers.nanfillstart(y, max(ylistlengths)) for y in ylist]
+                #Now stack results to look like as normal (time,variable,k)
+                self.yresult = N.dstack(ylist)
+                self.tresult = times
+                #Return klist to normal
+                self.k = klist
+            else:
+                times = N.arange(self.tstart, self.tend + self.tstep_wanted, self.tstep_wanted)
+                self.yresult = scipy_odeint(self.derivs, self.ystart, times)
+                self.tresult = times
         #Aggregrate results and calling parameters into results list
         self.lastparams = self.callingparams()
         
@@ -192,7 +226,7 @@ class CosmologicalModel(object):
                   "dxsav":self.dxsav,
                   "solver":self.solver,
                   "classname":self.__class__.__name__,
-                  "CVSRevision":"$Revision: 1.109 $",
+                  "CVSRevision":"$Revision: 1.110 $",
                   "datetime":datetime.datetime.now()
                   }
         return params
