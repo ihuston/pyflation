84d7e4ce2fae57332b6e0a759bde8c6eb06e8886
Ian Huston <ian@ianhuston.net>
2008-10-28 15:44:27 +0000

    Added SOCanonicalTwoStage

--- secondorder.py
+++ secondorder.py
@@ -1,5 +1,5 @@
 """Second Order Cosmological Model simulations by Ian Huston
-    $Id: secondorder.py,v 1.1 2008/10/28 15:34:21 ith Exp $
+    $Id: secondorder.py,v 1.2 2008/10/28 15:44:27 ith Exp $
     
     Provides generic class CosmologicalModel that can be used as a base for explicit models."""
 
@@ -8,7 +8,7 @@ import numpy as N
 from pdb import set_trace
 import cmpotentials
 
-class CanonicalFirstOrder(PhiModels):
+class CanonicalSecondOrder(PhiModels):
     """First order model using efold as time variable.
        y[0] - \phi_0 : Background inflaton
        y[1] - d\phi_0/d\eta : First deriv of \phi
@@ -17,6 +17,10 @@ class CanonicalFirstOrder(PhiModels):
        y[4] - \delta\varphi_1^\prime : Derivative of first order perturbation [Real Part]
        y[5] - \delta\varphi_1 : First order perturbation [Imag Part]
        y[6] - \delta\varphi_1^\prime : Derivative of first order perturbation [Imag Part]
+       y[7] - \delta\varphi_2 : Second order perturbation [Real Part]
+       y[8] - \delta\varphi_2^\prime : Derivative of second order perturbation [Real Part]
+       y[9] - \delta\varphi_2 : Second order perturbation [Imag Part]
+       y[10] - \delta\varphi_2^\prime : Derivative of second order perturbation [Imag Part]
        """
     def __init__(self,  k=None, ainit=None, *args, **kwargs):
         """Initialize variables and call superclass"""
@@ -37,7 +41,7 @@ class CanonicalFirstOrder(PhiModels):
         
         #Initial conditions for each of the variables.
         if self.ystart is None:
-            self.ystart = N.array([15.0,-0.1,0.0,1.0,0.0,1.0,0.0])   
+            self.ystart = N.array([15.0,-0.1,0.0,1.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0])
         
         #Set initial H value if None
         if N.all(self.ystart[2] == 0.0):
@@ -53,7 +57,11 @@ class CanonicalFirstOrder(PhiModels):
                         r"Real $\delta\varphi_1$",
                         r"Real $\dot{\delta\varphi_1}$",
                         r"Imag $\delta\varphi_1$",
-                        r"Imag $\dot{\delta\varphi_1}$"]
+                        r"Imag $\dot{\delta\varphi_1}$",
+                        r"Real $\delta\varphi_2$",
+                        r"Real $\dot{\delta\varphi_2}$",
+                        r"Imag $\delta\varphi_2$",
+                        r"Imag $\dot{\delta\varphi_2}$"]
                     
     def derivs(self, y, t):
         """Basic background equations of motion.
@@ -64,9 +72,9 @@ class CanonicalFirstOrder(PhiModels):
         
         #Set derivatives taking care of k type
         if type(self.k) is N.ndarray or type(self.k) is list: 
-            dydx = N.zeros((7,len(self.k)))
+            dydx = N.zeros((11,len(self.k)))
         else:
-            dydx = N.zeros(7)
+            dydx = N.zeros(11)
             
         
         #d\phi_0/dn = y_1
@@ -95,5 +103,92 @@ class CanonicalFirstOrder(PhiModels):
         #
         dydx[6] = (-(3 + dydx[2]/y[2])*y[6]  - ((self.k/(a*y[2]))**2)*y[5]
                     -(d2Udphi2 + 2*y[1]*dUdphi + (y[1]**2)*U)*(y[5]/(y[2]**2)))
+        #
+        #Second Order perturbations
+        #
+        #d\deltaphi_2/dn Real
+        dydx[7] = y[8]
+        
+        #d\deltaphi_2^\prime/dn Real
+        dydx[8] = (-(3 + dydx[2]/y[2])*y[8] - ((self.k/(a*y[2]))**2)*y[7]
+                    -(d2Udphi2 + 2*y[1]*dUdphi + (y[1]**2)*U)*(y[7]/(y[2]**2)))
+        
+        #d\deltaphi_2/dn Imag
+        dydx[9] = y[10]
+        
+        #d\deltaphi_2^\prime/dn Imag
+        dydx[10] = (-(3 + dydx[2]/y[2])*y[10] - ((self.k/(a*y[2]))**2)*y[9]
+                    -(d2Udphi2 + 2*y[1]*dUdphi + (y[1]**2)*U)*(y[9]/(y[2]**2)))
         
         return dydx
+        
+        
+class SOCanonicalTwoStage(CanonicalTwoStage):
+    """Implementation of Second Order Canonical two stage model with standard initial conditions for phi.
+    """
+                    
+    def __init__(self, *args, **kwargs):
+        """Initialize model and ensure initial conditions are sane."""
+        
+        #Initial conditions for each of the variables.
+        if kwargs["ystart"] is None:
+            #Initial conditions for all variables
+            self.ystart = kwargs["ystart"] = N.array([18.0, # \phi_0
+                                   -0.1, # \dot{\phi_0}
+                                    0.0, # H - leave as 0.0 to let program determine
+                                    1.0, # Re\delta\phi_1
+                                    0.0, # Re\dot{\delta\phi_1}
+                                    1.0, # Im\delta\phi_1
+                                    0.0  # Im\dot{\delta\phi_1}
+                                    ])
+        else:
+            self.ystart = kwargs["ystart"]
+        
+        #Call superclass
+        super(FOCanonicalTwoStage, self).__init__(*args, **kwargs)
+        
+    def getfoystart(self):
+        """Model dependent setting of ystart"""
+        #Reset starting conditions at new time
+        foystart = N.zeros((len(self.ystart), len(self.k)))
+        
+        #set_trace()
+        #Get values of needed variables at crossing time.
+        astar = self.ainit*N.exp(self.fotstart)
+        Hstar = self.bgmodel.yresult[self.fotstartindex,2]
+        epsstar = self.bgepsilon[self.fotstartindex]
+        etastar = -1/(astar*Hstar*(1-epsstar))
+        etainit = -1/(self.ainit*self.bgmodel.yresult[0,2]*(1-self.bgepsilon[0]))
+        etadiff = etastar - etainit
+        keta = self.k*etadiff
+        
+        #Set bg init conditions based on previous bg evolution
+        foystart[0:3] = self.bgmodel.yresult[self.fotstartindex,:].transpose()
+        
+        #Find 1/asqrt(2k)
+        arootk = 1/(astar*(N.sqrt(2*self.k)))
+        #Find cos and sin(-keta)
+        csketa = N.cos(-keta)
+        snketa = N.sin(-keta)
+        
+        #Set Re\delta\phi_1 initial condition
+        foystart[3,:] = csketa*arootk
+        #set Re\dot\delta\phi_1 ic
+        foystart[4,:] = -arootk*(csketa - (self.k/(astar*Hstar))*snketa)
+        #Set Im\delta\phi_1
+        foystart[5,:] = snketa*arootk
+        #Set Im\dot\delta\phi_1
+        foystart[6,:] = -arootk*((self.k/(astar*Hstar))*csketa + snketa)
+        
+        return foystart
+    
+    def getdeltaphi(self):
+        """Find the spectrum of perturbations for each k. 
+           Return Pr.
+           """
+        #Raise error if first order not run yet
+        self.checkfirstordercomplete()
+        
+        #Set nice variable names
+        deltaphi = self.yresult[:,3,:] + self.yresult[:,5,:]*1j #complex deltaphi
+        return deltaphi
