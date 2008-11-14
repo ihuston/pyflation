f18d87ad5a5a1bbc050716574c9f45ff7562236d
Ian Huston <ian@ianhuston.net>
2008-11-14 17:28:29 +0000

    Completed skeleton of getsourceintegrand. Now need term2 and 3.

--- sosource.py
+++ sosource.py
@@ -1,32 +1,52 @@
 """Second order helper functions to set up source term
-    $Id: sosource.py,v 1.3 2008/11/14 14:04:05 ith Exp $
+    $Id: sosource.py,v 1.4 2008/11/14 17:28:29 ith Exp $
     """
-import numpy as N
+
 from __future__ import division # Get rid of integer division problems, i.e. 1/2=0
 import tables
+import numpy as N
+from scipy import integrate
+import helpers
   
 def getmodelfromfile(filename):
     """Return model having read model data from a file"""
-    pass
+    pass      
     
-def getsourceterm(m):
+def getsourceintegrand(m):
     """Return source term (slow-roll for now), once first order system has been executed."""
     #Make sure first order is complete
     m.checkfirstordercomplete()
     
+    #Get first order ICs:
+    nanfiller = [m.getfoystart(m.tresult[tix], [tix]) for tix in xrange(len(m.tresult))]
+    nanfiller = N.array(nanfiller)
+    
+    #switch nans for ICs in m.yresult
+    myr = m.yresult.copy()
+    are_nan = N.isnan(myr)
+    myr[are_nan] = nanfiller[are_nan]
+    
     #Get first order results (from file or variables)
-    phi, phidot, H, dphi1real, dphi1dotreal, dphi1imag, dphi1dotimag = m.yresult[:,i,:] for i in range(7)
+    phi, phidot, H, dphi1real, dphi1dotreal, dphi1imag, dphi1dotimag = [myr[:,i,:] for i in range(7)]
     dphi1 = dphi1real + dphi1imag*1j
     dphi1dot = dphi1dotreal + dphi1dotimag*1j
-    U, dU, dU2, dU3 = m.potentials()
+    pottuple = m.potentials(N.rollaxis(myr, 1))
+    #Get potentials in right shape
+    pt = []
+    for p in pottuple:
+        if N.shape(p) != N.shape(pottuple[0]):
+            pt.append(p*N.ones_like(pottuple[0]))
+        else:
+            pt.append(p)
+    U, dU, dU2, dU3 = pt
     
     #Initialize variables to store result
-    s2shape = (len(m.k),len(m.k)
+    s2shape = (len(m.k),len(m.k))
     source = []
     #Main loop over each time step
-    for nix, n in enumerate(m.fotresult):
+    for nix, n in enumerate(m.tresult):
         #Single time step
-        a = m.ainit*exp(n)
+        a = m.ainit*N.exp(n)
         
         #Initialize result variable for k modes
         s2 = N.empty(s2shape)
@@ -37,10 +57,30 @@ def getsourceterm(m):
             s1 = N.empty_like(m.k)
             for qix, q in enumerate(m.k):
                 #Single q mode
+                #Check abs(qix-kix)-1 is not negative
+                dphi1ix = N.abs(qix-kix) -1
+                if dphi1ix < 0:
+                    dp1temp = dp1dottemp = 0
+                else:
+                    dp1temp = dphi1[nix, dphi1ix]
+                    dp1dottemp = dphi1dot[nix, dphi1ix]
+                
                 #First major term:
-                term1 = 1/(2*N.pi**2) * (potentialterms) * q**2 *dphi1dot[t,N.abs(qix-kix+1)]
+                term1 = (1/(2*N.pi**2) * (1/H[nix,kix]**2) * (dU3[nix,kix] + 3*phidot[nix,kix]*dU2[nix,kix]) 
+                            * q**2*dp1temp*dp1dottemp)
+                term2 = 0
+                term3 = 0
                 s1[qix] = term1 + term2 + term3
             #add sourceterm for each q
             s2[kix] = s1
         #save results for each q
         source.append(s2)
+    source = N.array(source)
+    return source
+            
+def getsource(m):
+    """Return integrated source function for model m using romberg integration."""
+    if not helpers.ispower2(len(m.k)-1):
+        raise AttributeError("Need to have 2**n + 1 different k values for integration.")
+    msource = integrate.romb(getsourceintegrand(m))
+    return msource
