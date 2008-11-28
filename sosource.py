50f87fc1ac8fa9332c356d75d5dbf122d678db92
Ian Huston <ian@ianhuston.net>
2008-11-28 14:20:10 +0000

    Move time step to outer part of function. Start to integrate hdf5 file opening.

--- sosource.py
+++ sosource.py
@@ -1,5 +1,5 @@
 """Second order helper functions to set up source term
-    $Id: sosource.py,v 1.8 2008/11/28 12:27:22 ith Exp $
+    $Id: sosource.py,v 1.9 2008/11/28 14:20:10 ith Exp $
     """
 
 from __future__ import division # Get rid of integer division problems, i.e. 1/2=0
@@ -16,35 +16,36 @@ def getsourceintegrand(m):
     """Return source term (slow-roll for now), once first order system has been executed."""
     #Make sure first order is complete
     m.checkfirstordercomplete()
-    
-    #Get first order ICs:
-    nanfiller = [m.getfoystart(m.tresult[tix], [tix]) for tix in xrange(len(m.tresult))]
-    nanfiller = N.array(nanfiller)
-    
-    #switch nans for ICs in m.yresult
-    myr = m.yresult.copy()
-    are_nan = N.isnan(myr)
-    myr[are_nan] = nanfiller[are_nan]
-    
-    #Get first order results (from file or variables)
-    phi, phidot, H, dphi1real, dphi1dotreal, dphi1imag, dphi1dotimag = [myr[:,i,:] for i in range(7)]
-    dphi1 = dphi1real + dphi1imag*1j
-    dphi1dot = dphi1dotreal + dphi1dotimag*1j
-    pottuple = m.potentials(N.rollaxis(myr, 1))
-    #Get potentials in right shape
-    pt = []
-    for p in pottuple:
-        if N.shape(p) != N.shape(pottuple[0]):
-            pt.append(p*N.ones_like(pottuple[0]))
-        else:
-            pt.append(p)
-    U, dU, dU2, dU3 = pt
-    
-    #Initialize variables to store result
-    s2shape = (len(m.k),len(m.k))
+    #Set up file for results
     source = []
+    #Initialize variables to store result
+    lenmk = len(m.k)
+    s2shape = (lenmk,lenmk)
     #Main loop over each time step
-    for nix, n in enumerate(m.tresult):
+    for nix, n in enumerate(m.tresult):    
+        #Get first order ICs:
+        nanfiller = m.getfoystart(m.tresult[nix], [nix])
+        #nanfiller = N.array(nanfiller)
+        
+        #switch nans for ICs in m.yresult
+        myr = m.yresult[nix].copy()
+        are_nan = N.isnan(myr)
+        myr[are_nan] = nanfiller[are_nan]
+        
+        #Get first order results (from file or variables)
+        phi, phidot, H, dphi1real, dphi1dotreal, dphi1imag, dphi1dotimag = [myr[nix,i,:] for i in range(7)]
+        dphi1 = dphi1real + dphi1imag*1j
+        dphi1dot = dphi1dotreal + dphi1dotimag*1j
+        pottuple = m.potentials(N.rollaxis(myr, 1))
+        #Get potentials in right shape
+        pt = []
+        for p in pottuple:
+            if N.shape(p) != N.shape(pottuple[0]):
+                pt.append(p*N.ones_like(pottuple[0]))
+            else:
+                pt.append(p)
+        U, dU, dU2, dU3 = pt
+        
         #Single time step
         a = m.ainit*N.exp(n)
         
@@ -52,7 +53,6 @@ def getsourceintegrand(m):
         s2 = N.empty(s2shape)
         for kix, k in enumerate(m.k):
             #Single k mode
-            
             #Result variable for source
             s1 = N.empty_like(m.k)
             for qix, q in enumerate(m.k):
@@ -62,17 +62,17 @@ def getsourceintegrand(m):
                 if dphi1ix < 0:
                     dp1diff = dp1dotdiff = 0
                 else:
-                    dp1diff = dphi1[nix, dphi1ix]
-                    dp1dotdiff = dphi1dot[nix, dphi1ix]
+                    dp1diff = dphi1[dphi1ix]
+                    dp1dotdiff = dphi1dot[dphi1ix]
                 
                 #First major term:
-                term1 = (1/(2*N.pi**2) * (1/H[nix,kix]**2) * (dU3[nix,kix] + 3*phidot[nix,kix]*dU2[nix,kix]) 
-                            * q**2*dp1diff*dphi1[nix,qix])
+                term1 = (1/(2*N.pi**2) * (1/H[kix]**2) * (dU3[kix] + 3*phidot[kix]*dU2[kix]) 
+                            * q**2*dp1diff*dphi1[qix])
                 #Second major term:
-                term2 = (1/(2*N.pi**2) * ((1/(a*H[nix,kix]) + 0.5)*q**2 - 2*(q**4/k**2)) * dp1dotdiff * dphi1dot[nix,qix])
+                term2 = (1/(2*N.pi**2) * ((1/(a*H[kix]) + 0.5)*q**2 - 2*(q**4/k**2)) * dp1dotdiff * dphi1dot[qix])
                 #Third major term:
-                term3 = (1/(2*N.pi**2) * 1/(a*H[nix,kix])**2 * (2*(q**6/k**2) + 2.5*q**4 + 2*(k*q)**2) * phidot[nix,kix] 
-                            * dp1diff * dphi1[nix,qix])
+                term3 = (1/(2*N.pi**2) * 1/(a*H[kix])**2 * (2*(q**6/k**2) + 2.5*q**4 + 2*(k*q)**2) * phidot[kix] 
+                            * dp1diff * dphi1[qix])
                 s1[qix] = term1 + term2 + term3
             #add sourceterm for each q
             s2[kix] = s1
