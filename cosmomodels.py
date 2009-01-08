96df048065ae79e65f8290c4e3b2e5a824a8cc53
Ian Huston <ian@ianhuston.net>
2009-01-08 15:21:07 +0000

    Fix indent.

--- cosmomodels.py
+++ cosmomodels.py
@@ -1,5 +1,5 @@
 """Cosmological Model simulations by Ian Huston
-    $Id: cosmomodels.py,v 1.197 2009/01/08 15:20:04 ith Exp $
+    $Id: cosmomodels.py,v 1.198 2009/01/08 15:21:07 ith Exp $
     
     Provides generic class CosmologicalModel that can be used as a base for explicit models."""
 
@@ -266,7 +266,7 @@ class CosmologicalModel(object):
                   "dxsav":self.dxsav,
                   "solver":self.solver,
                   "classname":self.__class__.__name__,
-                  "CVSRevision":"$Revision: 1.197 $",
+                  "CVSRevision":"$Revision: 1.198 $",
                   "datetime":datetime.datetime.now().strftime("%Y%m%d%H%M%S")
                   }
         return params
@@ -1068,7 +1068,7 @@ class MultiStageModel(CosmologicalModel):
                   "dxsav":self.dxsav,
                   "solver":self.solver,
                   "classname":self.__class__.__name__,
-                  "CVSRevision":"$Revision: 1.197 $",
+                  "CVSRevision":"$Revision: 1.198 $",
                   "datetime":datetime.datetime.now().strftime("%Y%m%d%H%M%S")
                   }
         return params
@@ -1448,14 +1448,14 @@ def make_wrapper_model(modelfile, *args, **kwargs):
     try:
         self._log.debug("Opening file " + modelfile + " to read class name.")
         rf = tables.openFile(modelfile, "r")
+        try:
             try:
-                try:
-                    params = self._rf.root.results.params
-                    modelclass = params[0]["classname"]
-                except tables.NoSuchNodeError:
-                    raise ModelError("File does not contain correct model data structure!")
-            finally:
-                rf.close()
+                params = self._rf.root.results.params
+                modelclass = params[0]["classname"]
+            except tables.NoSuchNodeError:
+                raise ModelError("File does not contain correct model data structure!")
+        finally:
+            rf.close()
     except IOError:
         raise
                 
