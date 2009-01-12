e70f57b2471565f1564063ab246120ec3cb6392b
Ian Huston <ian@ianhuston.net>
2009-01-12 14:45:20 +0000

    Helper functions for second order data.

---
+++ sohelpers.py
@@ -0,0 +1,6 @@
+"""Second order helper functions by Ian Huston
+    $Id: sohelpers.py,v 1.1 2009/01/12 14:45:20 ith Exp $
+    
+    Provides helper functions for second order data from cosmomodels.py"""
+    
+import tables
