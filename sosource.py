605701968084f407b5116a1e49b0ca83760ed065
Ian Huston <ian@ianhuston.net>
2009-01-28 11:48:50 +0000

    Removed old getsource functions and expanded getsourceandintegrate docstring.

--- sosource.py
+++ sosource.py
@@ -1,5 +1,5 @@
 """Second order helper functions to set up source term
-    $Id: sosource.py,v 1.28 2009/01/12 18:47:59 ith Exp $
+    $Id: sosource.py,v 1.29 2009/01/28 11:48:50 ith Exp $
     """
 
 from __future__ import division # Get rid of integer division problems, i.e. 1/2=0
@@ -16,159 +16,33 @@ RESULTSDIR = "/misc/scratch/ith/numerics/results/"
 
 #Start logging
 source_logger = logging.getLogger(__name__)
-  
-def getsourceintegrand(m, savefile=None):
-    """Return source term (slow-roll for now), once first order system has been executed."""
-    #Initialize variables to store result
-    lenmk = len(m.k)
-    s2shape = (lenmk, lenmk)
-    source_logger.debug("Shape of m.k is %s.", str(lenmk))
-    #Get atom shape for savefile
-    atomshape = (0, lenmk, lenmk)
-    
-    #Set up file for results
-    if not savefile or not os.path.isdir(os.path.dirname(savefile)):
-        date = time.strftime("%Y%m%d%H%M%S")
-        savefile = RESULTSDIR + "source" + date + ".hf5"
-        source_logger.info("Saving source results in file " + savefile)
-
-    #Main try block for file IO
-    try:
-        sf, sarr = opensourcefile(savefile, atomshape, sourcetype="int")
-        try:
-            source_logger.debug("Entering main time loop...")    
-            #Main loop over each time step
-            for nix, n in enumerate(m.tresult):    
-                if nix%100 == 0:
-                    source_logger.info("Starting n=%f, nix=%f sequence...", n, nix)
-                #Get first order ICs:
-                nanfiller = m.getfoystart(m.tresult[nix].copy(), N.array([nix]))
-                source_logger.debug("Left getfoystart. Filling nans...")
-                #switch nans for ICs in m.yresult
-                myr = m.yresult[nix].copy()
-                are_nan = N.isnan(myr)
-                myr[are_nan] = nanfiller[are_nan]
-                source_logger.debug("NaNs filled. Setting dynamical variables...")
-                
-                #Get first order results (from file or variables)
-                phi, phidot, H, dphi1real, dphi1dotreal, dphi1imag, dphi1dotimag = [myr[i,:] for i in range(7)]
-                dphi1 = dphi1real + dphi1imag*1j
-                dphi1dot = dphi1dotreal + dphi1dotimag*1j
-                source_logger.debug("Variables set. Getting potentials for this timestep...")
-                pottuple = m.potentials(myr)
-                #Get potentials in right shape
-                pt = []
-                for p in pottuple:
-                    if N.shape(p) != N.shape(pottuple[0]):
-                        pt.append(p*N.ones_like(pottuple[0]))
-                    else:
-                        pt.append(p)
-                U, dU, dU2, dU3 = pt
-                source_logger.debug("Potentials obtained. Setting a and making results array...")
-                #Single time step
-                a = m.ainit*N.exp(n)
-                
-                #Initialize result variable for k modes
-                s2 = N.empty(s2shape)
-                
-                source_logger.debug("Starting main k loop...")
-                #Get k indices
-                kix = N.arange(lenmk)
-                qix = N.arange(lenmk)
-                #Check abs(qix-kix)-1 is not negative
-                dphi1ix = N.abs(qix[:, N.newaxis]-kix) -1
-                dp1diff = N.where(dphi1ix < 0, 0, dphi1[dphi1ix])
-                dp1dotdiff = N.where(dphi1ix <0, 0, dphi1dot[dphi1ix])                    
-                #temp k and q vars
-                k = m.k[kix]
-                q = m.k[qix]
-                #First major term:
-                s2 = (1/(2*N.pi**2) * (1/H**2) * (dU3 + 3*phidot*dU2) 
-                            * q**2*dp1diff*dphi1)
-                #Second major term:
-                s2 += (1/(2*N.pi**2) * ((1/(a*H) + 0.5)*q**2 - 2*(q**4/k**2)) * dp1dotdiff * dphi1dot)
-                #Third major term:
-                s2 += (1/(2*N.pi**2) * 1/(a*H)**2 * (2*(q**6/k**2) + 2.5*q**4 + 2*(k*q)**2) * phidot
-                                            * dp1diff * dphi1)
-                #save results for each q
-                source_logger.debug("Saving results for this tstep...")
-                sarr.append(s2[N.newaxis])
-                source_logger.debug("Results for this tstep saved.")
-        finally:
-            #source = N.array(source)
-            sf.close()
-    except IOError:
-        raise
-    return savefile
-            
-def getsource(intfile=None, savefile=None, intmethod=None, fullinfo=False):
-    """Return integrated source function for model m using romberg integration."""
-    if intfile is None:
-        raise ValueError("Need to specify source file.")
-    #open integrand file
-    try:
-        intf = tables.openFile(intfile, "r")
-        try:
-            try:
-                iarr = intf.root.results.sourceint
-                k = intf.root.results.k
-            except tables.NoSuchNodeError:
-                source_logger.exception("Integrand data file is not in the correct format!")
-                raise            
-            #Shape of results should be (0, len(k))
-            source_logger.debug("k shape is %s", str(len(k)))
-            atomshape = iarr[0:0].shape[:-1]
-            try:
-                #Open file to save to
-                source_logger.debug("Trying to open source term save file %s", savefile)
-                sf, sarr = opensourcefile(savefile, atomshape, sourcetype="term")
-                try:
-                    #Choose integration method
-                    if intmethod is None:
-                        try:
-                            if all(N.diff(k) == k[1]-k[0]) and helpers.ispower2(len(k)-1):
-                                intmethod = "romb"
-                            else:
-                                intmethod = "simps"
-                        except IndexError:
-                                raise IndexError("Need more than one k to calculate integral!")
-                    #Now proceed with integration
-                    if intmethod is "romb":
-                        if not helpers.ispower2(len(k)-1):
-                            raise AttributeError("Need to have 2**n + 1 different k values for integration.")
-                        intfunc = integrate.romb
-                        fnargs = []
-                    elif intmethod is "simps":
-                        intfunc = integrate.simps
-                        fnargs = [k]
-                    else:
-                        raise ValueError("Need to specify correct integration method!")
-                    #Log integration method
-                    source_logger.debug("Integration method chosen is %s.", intmethod)
-                    
-                    #Do main loop over rows
-                    source_logger.info("Starting main integration loop.")
-                    for row in iarr:
-                        sarr.append(intfunc(row, *fnargs)[N.newaxis,:])
-                    source_logger.info("Integration loop complete.")
-                finally:
-                    #Close savefile
-                    sf.close()
-                    source_logger.debug("Source (save) file closed.")
-            except IOError:
-                source_logger.exception("Error opening source term save file!")
-                raise
-        finally:
-            #Close integrand file
-            intf.close()
-            source_logger.debug("Integrand file closed.")
-    except IOError:
-        source_logger.exception("IO Error during process.")
-        raise
-    return savefile
 
 def getsourceandintegrate(m, savefile=None, intmethod=None):
-    """Return source term (slow-roll for now), once first order system has been executed."""
+    """Calculate and save integrated source term.
+    
+    Using first order results in the specified model, the source term for second order perturbations 
+    is obtained from the given source function. The convolution integral is performed and the results
+    are saved in a file with the specified filename.
+    
+    Parameters
+    ----------
+    m: compatible cosmomodels model instance
+       The model class should contain first order results as in `cosmomodels.FOCanonicalTwoStage`
+    
+    savefile: String, optional
+              Filename where results should be saved.
+    
+    intmethod: {"romb", "simps"}
+               Two different integration methods to perform convolution integral with. If neither is
+               specified the length of the `k` array is checked to see whether the preferred choice
+               of romberg integration is possible. Integration methods are `numpy.integrate.romb` and
+               `numpy.integrate.simps`.
+               
+    Returns
+    -------
+    savefile: String
+              Filename where results have been saved.
+    """
     #testing
 #     nixend = 10
     #Initialize variables to store result
