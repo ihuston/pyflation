193d7bf61589d85a520cdb4da70de0bda7999fd4
Ian Huston <ian@ianhuston.net>
2009-03-19 15:33:54 +0000

    Add in plotresults function from cosmomodels.

--- cosmographs.py
+++ cosmographs.py
@@ -6,6 +6,12 @@ Graphing functions for cosmomodels
 """
 import pylab as P
 import os
+import helpers
+
+class CosmoGraphError(StandardError):
+    """Generic error for graphing facilities."""
+    pass
+    
 
 def multi_format_save(filenamestub, fig=None, formats=None, **kwargs):
     """Save figure in multiple formats at once.
@@ -52,4 +58,41 @@ def multi_format_save(filenamestub, fig=None, formats=None, **kwargs):
             raise
         savedfiles.append(filename)
     return savedfiles
+    
+def plotresults(m, fig=None, show=True, varindex=None, klist=None, saveplot=False, numks=5):
+    """Plot results of simulation run on a graph.
+        Return figure instance used."""
+    if m.runcount == 0:
+        raise CosmoGraphError("Model has not been run yet, cannot plot results!")
+    
+    if varindex is None:
+        varindex = 0 #Set default list of variables to plot
+    
+    if fig is None:
+        fig = P.figure() #Create figure
+    else:
+        P.figure(fig.number)
+    #One plot command for with ks, one for without
+    
+    if m.k is None:
+        P.plot(m.tresult, m.yresult[:,varindex])
+    else:
+        if klist is None:
+            klist = slice(0, len(m.k), len(m.k)/numks)
+        P.plot(m.tresult, m.yresult[:,varindex,klist])
+    #Create legends and axis names
+    P.xlabel(m.tname)
+    P.ylabel(m.ynames[varindex])
+    if klist is not None:
+        P.legend([r"$k=" + helpers.eto10(ks) + "$" for ks in m.k[klist]])
+    #P.title(m.plottitle, figure=fig)
+    
+    #Should we show it now or just return it without showing?
+    if show:
+        P.show()
+    #Should we save the plot somewhere?
+    if saveplot:
+        m.saveplot(fig)   
+    #Return the figure instance
+    return fig
     
