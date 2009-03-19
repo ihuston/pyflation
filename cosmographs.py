bee849231516ff07b559d61005d3e68898926ef9
Ian Huston <ian@ianhuston.net>
2009-03-19 15:19:45 +0000

    Initial version of plotting functions. Moved multi_format_save from plothelp.py.

---
+++ cosmographs.py
@@ -0,0 +1,55 @@
+"""cosmographs.py
+
+Author: Ian Huston
+
+Graphing functions for cosmomodels
+"""
+import pylab as P
+import os
+
+def multi_format_save(filenamestub, fig=None, formats=None, **kwargs):
+    """Save figure in multiple formats at once.
+    
+    Parameters
+    ----------
+
+    filenamestub: String
+                  Filename with path to which will be added the appropriate 
+                  extension for each different file saved.
+                  
+    fig: Matplotlib figure object, optional
+         Figure to save to disk. Defaults to current figure.
+         
+    formats: list-like, optional
+             List of format specifiers to save to,
+             default is ["pdf", "eps", "png", "svg"]
+             Must be of types supported by current installation.
+             
+    Other kwargs: These are provided to matplotlib.pylab.savefig.
+                  See there for documentation.
+    
+    Returns
+    -------
+    filenames: list
+               list of saved file names         
+    """
+    if not formats:
+        formats = ["png", "pdf", "eps", "svg"]
+    #Check directory exists
+    if not os.path.isdir(os.path.dirname(filenamestub)):
+        raise IOError("Directory specified does not exist!")
+    if not fig:
+        fig = P.gcf()
+    #Check files don't exist
+    savedfiles = []
+    for f in formats:
+        filename = filenamestub + "." + f
+        if os.path.isfile(filename):
+            raise IOError("File " + filename + " already exists! File not overwritten.")
+        try:
+            fig.savefig(filename, format = f, **kwargs)
+        except IOError:
+            raise
+        savedfiles.append(filename)
+    return savedfiles
+    
