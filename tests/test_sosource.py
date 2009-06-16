21e8d7f8e009d0e4652ddb7f78f2481934fdbe27
Ian Huston <ian@ianhuston.net>
2009-06-16 15:29:45 +0000

    Tests for sosource.py

---
+++ tests/test_sosource.py
@@ -0,0 +1,13 @@
+"""Test suite for sosource.py
+
+Uses nosetest suite in numpy.testing
+"""
+from numpy.testing import *
+from sosource import *
+
+def test_klessq():
+    """Test klessq function"""
+    pass
+
+if __name__ == "__main__":
+    run_module_suite()
