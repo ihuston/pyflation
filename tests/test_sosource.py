2780c01a933c443ed38be59f8bea3143848e2144
Ian Huston <ian@ianhuston.net>
2009-06-17 15:45:04 +0000

    Added test generator.

--- tests/test_sosource.py
+++ tests/test_sosource.py
@@ -3,48 +3,44 @@
 Uses nosetest suite in numpy.testing
 """
 import numpy as np
-from numpy.testing import *
+from numpy.testing import assert_array_almost_equal
 import sosource
 
+fixtures = [
+    {"kmin": 0.001, "fullkmax": 1.026, "deltak": 0.001, "numsoks": 513, "nthetas": 129, "A": 1, "B": 1},
+    {"kmin": 1.000, "fullkmax": 2.025, "deltak": 0.001, "numsoks": 513, "nthetas": 129, "A": 1, "B": 1}    
+]
 
-def test_klessq():
-    """Test klessq function"""
-    #Setup, move to another func if necessary
-    kmin = 0.001
-    fullkmax = 1.026
-    deltak = 0.001
-    numsoks = 513
-    nthetas = 129
+def check_klessq(fixture):
+    """Do checking of klessq versus analytical result."""
     #Init vars
-    fullk = np.arange(kmin, fullkmax+deltak, deltak)
-    k = q = fullk[:numsoks]
-    k2 = k[..., np.newaxis]
-    q2 = q[np.newaxis, ...]
-    theta = np.linspace(0,np.pi, nthetas)
+    fullk = np.arange(fixture["kmin"], fixture["fullkmax"] + fixture["deltak"], fixture["deltak"])
+    k = q = fullk[:fixture["numsoks"]]
+    km = k[..., np.newaxis] #k matrix
+    qm = q[np.newaxis, ...] #q matrix
+    theta = np.linspace(0, np.pi, fixture["nthetas"])
     
     #Get calculated array
     klq = np.array([sosource.klessq(onek, q, theta) for onek in k])
     
     #Test cases
-    assert_array_almost_equal(klq[:,:,-1], (k2 + q2))
-    assert_array_almost_equal(klq[:,:,0], abs(k2 - q2))    
+    assert_array_almost_equal(klq[:,:,-1], (km + qm))
+    assert_array_almost_equal(klq[:,:,0], abs(km - qm))
+    assert_array_almost_equal(klq[:,:, fixture["nthetas"]/2], np.sqrt(km**2 + qm**2))
+    
+def test_klessq():
+    """klessq values check"""
+    for fx in fixtures:
+        yield check_klessq, fx
 
-def check_conv():
+def get_convolution_envelope(fixture):
     """Check convolution using analytic solution. Not a test function."""
-    #Setup, move to another func if necessary
-    kmin = 0.001
-    fullkmax = 1.026
-    deltak = 0.001
-    numsoks = 513
-    nthetas = 129
-    A=1
-    B=1
     #Init vars
-    fullk = np.arange(kmin, fullkmax+deltak, deltak)
-    k = q = fullk[:numsoks]
-    k2 = k[..., np.newaxis]
-    q2 = q[np.newaxis, ...]
-    theta = np.linspace(0,np.pi, nthetas)
+    fullk = np.arange(fixture["kmin"], fixture["fullkmax"]+fixture["deltak"], fixture["deltak"])
+    k = q = fullk[:fixture["numsoks"]]
+    km = k[..., np.newaxis]
+    qm = q[np.newaxis, ...]
+    theta = np.linspace(0, np.pi, fixture["nthetas"])
     ie = k, q, theta
     
     dp1=A/sqrt(fullk)
@@ -54,10 +50,7 @@ def check_conv():
     klq = np.array([sosource.klessq(onek, q, theta) for onek in k])
     tterms=sosource.getthetaterms(ie, dp1, dp1dot)
     
-    ansoln= (2*A/(3*k2*q2)) * ((k2+q2)**1.5-(abs(k2-q2))**1.5)
+    ansoln= (2*A/(3*k2*q2)) * ((km+qm)**1.5-(abs(km-qm))**1.5)
     aterm=tterms[0,0]+tterms[0,1]*1j
     env=array([max(abs((ansoln[:,i]-aterm[:,i])/ansoln[:,i])) for i in arange(len(ansoln))])
     return env
-    
-if __name__ == "__main__":
-    run_module_suite()
