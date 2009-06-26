4404737b9fa864f45d569d5ce7b3391cb7de06cf
Ian Huston <ian@ianhuston.net>
2009-06-26 14:43:33 +0000

    Added convolution function.

--- sohelpers.py
+++ sohelpers.py
@@ -1,5 +1,5 @@
 """Second order helper functions by Ian Huston
-    $Id: sohelpers.py,v 1.9 2009/03/19 15:17:34 ith Exp $
+    $Id: sohelpers.py,v 1.10 2009/06/26 14:43:33 ith Exp $
     
     Provides helper functions for second order data from cosmomodels.py"""
     
@@ -9,6 +9,7 @@ import cosmomodels as c
 import logging
 import time
 import os.path
+from sosource import getthetaterms
 
 _log = logging.getLogger(__name__)
 
@@ -146,4 +147,75 @@ def combine_results(fofile, sofile, newfile=None):
     #Successful execution
     _log.debug("First and second order files successfully combined in %s.", newfile)
     return newfile
+   
+def soderivs_magnitude(m, y, t, **kwargs):
+    """Equation of motion for second order perturbations including source term"""
     
+    #Pick k from kwargs
+    k = kwargs["k"]
+    kix = kwargs["kix"]
+    
+    if kix is None:
+        raise ModelError("Need to specify kix in order to calculate 2nd order perturbation!")
+    #Need t index to use first order data
+    if kwargs["tix"] is None:
+        raise ModelError("Need to specify tix in order to calculate 2nd order perturbation!")
+    else:
+        tix = kwargs["tix"]
+
+    #Get first order results for this time step
+    fovars = m.yresult[tix,0:7].copy()[:,kix]
+    phi, phidot, H = fovars[0:3]
+    epsilon = m.bgepsilon[tix]
+    #get potential from function
+    U, dU, d2U, d3U = m.potentials(fovars, m.pot_params)[0:4]        
+    
+    #Set derivatives taking care of k type
+    if type(k) is N.ndarray or type(k) is list: 
+        dydx = N.zeros((4,len(k)))
+    else:
+        dydx = N.zeros(4)
+        
+    #Get a
+    a = m.ainit*N.exp(t)
+    #Real parts
+    #first term -(3 - epsilon)dp2dot
+    dydx[0] = -(3 - epsilon)*y[1]
+    
+    #second term dp2
+    dydx[1] = (- ((k/(a*H))**2)*y[0]
+                -(d2U/H**2 - 3*(phidot**2))*y[0])
+            
+    #Complex 1st term dp2dot
+    dydx[2] = -(3 - epsilon)*y[3]
+    
+    #Complex 2nd term dp2
+    dydx[3] = (- ((k/(a*H))**2)*y[2]
+                -(d2U/H**2 - 3*(phidot**2))*y[2])
+    
+    return dydx
+
+def find_soderiv_terms(m, kix=N.array([0])):
+    """Run through all time steps finding second order derivative terms."""
+    k=m.k[kix]
+    res = []
+    for tix, t in enumerate(m.tresult):
+        terms = soderivs_magnitude(m, m.yresult[tix, 7:,kix], t, tix=tix, kix=kix, k=k)
+        res.append(terms)
+    return N.array(res)
+    
+def get_selfconvolution(m, nix, nthetas=257, numsoks=1025):
+    """Return self convolution of given function."""
+    k = q = m.k[:numsoks]
+    theta = N.linspace(0, N.pi, nthetas)
+    ie = k, q, theta
+    dp1 = m.yresult[:,3] + m.yresult[:,5]*1j
+    dp1dot = m.yresult[:,4] + m.yresult[:,6]*1j
+    aterm = getthetaterms(ie, dp1, dp1dot)[0]
+    
+    qm = q[N.newaxis, ...]
+    dp1m = dp1[N,newaxis,...]
+    conv = 2*np.pi * qm**2 * dp1m * aterm
+    integrated = romb(aterm, k[1]-k[0])
+    return integrated  
+        
