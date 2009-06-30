a373818f78b12ae2be3db7990a6c506cb7fb1b43
Ian Huston <ian@ianhuston.net>
2009-06-30 18:25:14 +0000

    Use deepcopy to get around problems with ystart being overwritten.

--- findparams.py
+++ findparams.py
@@ -2,9 +2,11 @@
 from __future__ import division
 import cosmomodels as c
 import numpy as N
+from copy import deepcopy
 WMAP5PIVOT = N.array([5.25e-60])
+
 fixtures = {
-    "mass": {"name": "mass", "values":N.linspace(7e-6, 8e-6),
+    "mass": {"name": "mass", "values":N.linspace(6e-6, 7e-6),
              "pivotk":WMAP5PIVOT, "pot": "msqphisq",
              "ystart":N.array([18.0, # \phi_0
                         -0.1, # \dot{\phi_0}
@@ -28,11 +30,13 @@ fixtures = {
 
 def param_vs_spectrum(fixture, nefolds=5):
     """Run tests for a particular parameter, return mass used and spectrum after horizon exit"""
-    results = None        
-    for ps in fixture["values"]:
-        sim = c.FOCanonicalTwoStage(solver="rkdriver_withks", ystart=fixture["ystart"],
-                                    k=fixture["pivotk"], potential_func=fixture["pot"],
-                                    pot_params={fixture["name"]: ps},
+    results = None
+    fx = deepcopy(fixture)
+    fxystart = fx["ystart"]
+    for ps in fx["values"]:
+        sim = c.FOCanonicalTwoStage(solver="rkdriver_withks", ystart=fxystart.copy(),
+                                    k=fx["pivotk"], potential_func=fx["pot"],
+                                    pot_params={fx["name"]: ps},
                                     tend=83, quiet=True)
         
         
@@ -40,15 +44,15 @@ def param_vs_spectrum(fixture, nefolds=5):
         scaledPr = sim.k**3/(2*N.pi**2)*sim.Pr
         tres = sim.findHorizoncrossings()[:,0] + nefolds/sim.tstep_wanted
         Pres = scaledPr[tres.astype(int)].diagonal()[0]
-        print "Running model with %s=%s gives scaledPr=%s"%(fixture["name"], str(ps), str(Pres))
+        print "Running model with %s=%s gives scaledPr=%s"%(fx["name"], str(ps), str(Pres))
         if results is not None:
             results = N.vstack((results, N.array([ps, Pres])))
         else:
             results = N.array([ps, Pres])
-        del sim
+        del sim, tres, Pres
     
     return results
 
 
 if __name__ == "__main__":
-    print param_vs_spectrum(fixtures["mass"])
+    print param_vs_spectrum(fixtures["mass"].copy())
