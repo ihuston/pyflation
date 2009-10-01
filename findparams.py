41b88b39e1bd0185ede4ba954d8e78b2562053b9
Ian Huston <ian@ianhuston.net>
2009-10-01 10:31:12 +0000

    Finished moving to full n-dimensional tester.

--- findparams.py
+++ findparams.py
@@ -4,10 +4,12 @@ from __future__ import division
 import cosmomodels as c
 import numpy as N
 from copy import deepcopy
+import helpers
+
 WMAP5PIVOT = N.array([5.25e-60])
 
 fixtures = {
-    "mass": {"vars": "mass", "values":N.linspace(6.3265e-6, 6.3427e-6),
+    "mass": {"vars": ["mass"], "values":[N.linspace(6.3265e-6, 6.3427e-6)],
              "pivotk":WMAP5PIVOT, "pot": "msqphisq",
              "ystart":N.array([18.0, # \phi_0
                         -0.1, # \dot{\phi_0}
@@ -18,7 +20,7 @@ fixtures = {
                          0.0  # Im\dot{\delta\phi_1}
                          ])},
         #
-    "lambda": {"vars":"lambda", "values": N.linspace(1e-10, 1e-8), 
+    "lambda": {"vars":["lambda"], "values": [N.linspace(1e-10, 1e-8)], 
                "pivotk":WMAP5PIVOT, "pot": "lambdaphi4",
                "ystart": N.array([25.0,
                                   -1.0,
@@ -28,7 +30,7 @@ fixtures = {
                                   1.0,
                                   0])},
      #
-     "linde": {"vars":"lambda", "values": N.linspace(1.54e-13, 1.57e-13), 
+     "linde": {"vars":["lambda"], "values": [N.linspace(1.54e-13, 1.57e-13)], 
                "pivotk":WMAP5PIVOT, "pot": "linde",
                "ystart": N.array([25.0,
                                    0.0,
@@ -38,7 +40,7 @@ fixtures = {
                                    1.0,
                                    0])},
     #
-    "linde2": {"vars":"mass", "values": N.linspace(4.9e-8, 1e-6),
+    "linde2": {"vars":["mass"], "values": [N.linspace(4.9e-8, 1e-6)],
                 "pivotk":WMAP5PIVOT, "pot": "linde",
                 "ystart": N.array([25.0,
                                     0.0,
@@ -51,13 +53,15 @@ fixtures = {
 
 def param_vs_spectrum(fixture, nefolds=5):
     """Run tests for a particular parameter, return mass used and spectrum after horizon exit"""
+    #Variable to store results
     results = None
     fx = deepcopy(fixture)
     fxystart = fx["ystart"]
-    for ps in fx["values"]:
+    values_matrix = helpers.cartesian(fx["values"])
+    for ps in values_matrix:
         sim = c.FOCanonicalTwoStage(solver="rkdriver_withks", ystart=fxystart.copy(),
                                     k=fx["pivotk"], potential_func=fx["pot"],
-                                    pot_params={fx["vars"]: ps},
+                                    pot_params= dict(zip(fx["vars"], ps)),
                                     tend=83, quiet=True)
         
         
@@ -65,38 +69,13 @@ def param_vs_spectrum(fixture, nefolds=5):
         scaledPr = sim.k**3/(2*N.pi**2)*sim.Pr
         tres = sim.findHorizoncrossings()[:,0] + nefolds/sim.tstep_wanted
         Pres = scaledPr[tres.astype(int)].diagonal()[0]
-        print "Running model with %s=%s gives scaledPr=%s"%(fx["vars"], str(ps), str(Pres))
+        print "Running model with %s gives scaledPr=%s"%(str(dict(zip(fx["vars"], ps))), str(Pres))
         if results is not None:
-            results = N.vstack((results, N.array([ps, Pres])))
+            results = N.vstack((results, N.hstack([ps, Pres])))
         else:
-            results = N.array([ps, Pres])
+            results = N.hstack([ps, Pres])
         del sim, tres, Pres
     
-    return results
-    
-def param_vs_spectrum_3d(fixture, nefolds=5):
-    """Run tests for a particular parameter, return mass used and spectrum after horizon exit"""
-    results = None
-    fx = deepcopy(fixture)
-    fxystart = fx["ystart"]
-    for ps in fx["values"]:
-        sim = c.FOCanonicalTwoStage(solver="rkdriver_withks", ystart=fxystart.copy(),
-                                    k=fx["pivotk"], potential_func=fx["pot"],
-                                    pot_params={fx["vars"]: ps},
-                                    tend=83, quiet=True)
-        
-        
-        sim.run(saveresults=False)
-        scaledPr = sim.k**3/(2*N.pi**2)*sim.Pr
-        tres = sim.findHorizoncrossings()[:,0] + nefolds/sim.tstep_wanted
-        Pres = scaledPr[tres.astype(int)].diagonal()[0]
-        print "Running model with %s=%s gives scaledPr=%s"%(fx["vars"], str(ps), str(Pres))
-        if results is not None:
-            results = N.vstack((results, N.array([ps, Pres])))
-        else:
-                results = N.array([ps, Pres])
-        del sim, tres, Pres
-                
     return results
 
 
