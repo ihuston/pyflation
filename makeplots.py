8162e3f58fe125ef4f62a86260f3d854c21c2a9b
Ian Huston <ian@ianhuston.net>
2009-10-03 17:57:19 +0000

    Add plots.

--- makeplots.py
+++ makeplots.py
@@ -17,10 +17,9 @@ import os.path
 resdir = "/home/network/ith/results/analysis/newmass/"
 graphdir = os.path.join(resdir, "calN-graphs/")
 #files
-cmb=c.make_wrapper_model(os.path.join(resdir,"cmb-msqphisq-5e-62-1.0245e-58-1e-61.hf5"))
-fo=c.make_wrapper_model(os.path.join(resdir,"foandsrc-FOCanonicalTwoStage-msqphisq-5e-62-2.051e-58-1e-61-195554.hf5"))
-#k coefficient
-kc = cmb.k**(1.5)/(np.sqrt(2)*np.pi)
+cmbmsq=c.make_wrapper_model(os.path.join(resdir,"cmb-msqphisq-5e-62-1.0245e-58-1e-61.hf5"))
+fomsq=c.make_wrapper_model(os.path.join(resdir,"foandsrc-FOCanonicalTwoStage-msqphisq-5e-62-2.051e-58-1e-61-195554.hf5"))
+
 #texts
 calN=r"$\mathcal{N}_\mathrm{end} - \mathcal{N}$"
 
@@ -39,7 +38,7 @@ def save_with_prompt(fname):
 
 def set_size_small(fig):
     fig.set_size_inches((4,3))
-    fig.subplots_adjust(left=0.17, bottom=0.15, right=0.95, top=0.95)
+    fig.subplots_adjust(left=0.17, bottom=0.15, right=0.95, top=0.93)
     
 def set_size_large(fig):
     fig.set_size_inches((6,4.5))
@@ -53,13 +52,15 @@ def set_size(fig, size="large"):
     else:
         raise ValueError("Variable size should be either \"large\" or \"small\"!")
     
-def dp1_kwmap(fname="dp1_kwmap", size="large"):
+def dp1_kwmap(fname="dp1_kwmap", size="large", m=cmbmsq):
+    #k coefficient
+    kc = m.k**(1.5)/(np.sqrt(2)*np.pi)
     fig = P.figure()
     set_size(fig, size)
-    P.plot(cmb.tresult[-1]- cmb.tresult, kc[52]*cmb.dp1[:,52].real)
-    P.plot(cmb.tresult[-1]- cmb.tresult, kc[52]*cmb.dp1[:,52].imag, ls="--", color="g")
+    P.plot(m.tresult[-1]- m.tresult, kc[52]*m.dp1[:,52].real)
+    P.plot(m.tresult[-1]- m.tresult, kc[52]*m.dp1[:,52].imag, ls="--", color="g")
     ax = P.gca()
-    ax.axvline(cmb.tresult[-1]- 21.24, ls="-.", color="red")
+    ax.axvline(m.tresult[-1]- 21.24, ls="-.", color="red")
     cg.reversexaxis()
     #P.draw()
     P.xlabel(calN)
@@ -73,13 +74,15 @@ def dp1_kwmap(fname="dp1_kwmap", size="large"):
     
     return fig, fname
 
-def dp2_kwmap(fname="dp2_kwmap", size="small"):
+def dp2_kwmap(fname="dp2_kwmap", size="small", m=cmbmsq):
+    #k coefficient
+    kc = m.k**(1.5)/(np.sqrt(2)*np.pi)
     fig = P.figure()
     set_size(fig, size)
-    P.plot(cmb.tresult[-1]- cmb.tresult, kc[52]*cmb.dp2[:,52].real)
-    P.plot(cmb.tresult[-1]- cmb.tresult, kc[52]*cmb.dp2[:,52].imag, ls="--", color="g")
+    P.plot(m.tresult[-1]- m.tresult, kc[52]*m.dp2[:,52].real)
+    P.plot(m.tresult[-1]- m.tresult, kc[52]*m.dp2[:,52].imag, ls="--", color="g")
     ax = P.gca()
-    ax.axvline(cmb.tresult[-1]- 21.24, ls="-.", color="red")
+    ax.axvline(m.tresult[-1]- 21.24, ls="-.", color="red")
     cg.reversexaxis()
     #P.draw()
     ax.ticklabel_format(style="sci", axis="y", scilimits=(0,0))
@@ -90,9 +93,9 @@ def dp2_kwmap(fname="dp2_kwmap", size="small"):
     P.draw()
     return fig, fname
     
-def src_kwmap(fname="src-kwmap", size="small"):
+def src_kwmap(fname="src-kwmap", size="small", m=cmbmsq, fo=fomsq):
     #Get timeresult for correct range
-    tr52 = cmb.tresult[-1] - cmb.tresult[865:]
+    tr52 = m.tresult[-1] - m.tresult[865:]
     #Get source values at correct timesteps (every second one) for kwmap
     s52 = np.abs(fo.source[::2,52])
     #Create figure and set size
@@ -109,14 +112,14 @@ def src_kwmap(fname="src-kwmap", size="small"):
     P.draw()
     return fig, fname
     
-def bgepsilon(fname="bgepsilon", size="large"):
-    bgeps = cmb.bgepsilon
+def bgepsilon(fname="bgepsilon", size="large", m=cmbmsq):
+    bgeps = m.bgepsilon
     fig = P.figure()
     set_size(fig, size)
     
-    P.plot(cmb.bgmodel.tresult, bgeps)
+    P.plot(m.bgmodel.tresult, bgeps)
     ax = P.gca()
-    ax.axvline(cmb.tresult[-1], ls="--", color="red")
+    ax.axvline(m.tresult[-1], ls="--", color="red")
     P.xlabel(r"$\mathcal{N}$")
     P.ylabel(r"$\varepsilon_H$")
     ax.set_xlim((78.8, 83.2))
@@ -124,15 +127,15 @@ def bgepsilon(fname="bgepsilon", size="large"):
     P.draw()
     return fig, fname
     
-def pr_kwmap(fname="Pr-kwmap", size="small", horiz=True):
-    Pr = cmb.Pr
-    scPr = cmb.k[52]**3/(2*np.pi**2) * Pr[:,52]
+def pr_kwmap(fname="Pr-kwmap", size="small", horiz=True, m=cmbmsq):
+    Pr = m.Pr
+    scPr = m.k[52]**3/(2*np.pi**2) * Pr[:,52]
     fig = P.figure()
     set_size(fig, size)
-    P.semilogy(cmb.tresult[-1]-cmb.tresult[865:], np.abs(scPr[865:]))
+    P.semilogy(m.tresult[-1]-m.tresult[865:], np.abs(scPr[865:]))
     ax = P.gca()
     if horiz:
-        ax.axvline(cmb.tresult[-1] - 21.24, ls="--", color="red")
+        ax.axvline(m.tresult[-1] - 21.24, ls="--", color="red")
     cg.reversexaxis()
     
     P.xlabel(calN)
@@ -140,15 +143,15 @@ def pr_kwmap(fname="Pr-kwmap", size="small", horiz=True):
     P.draw()
     return fig, fname
     
-def pphi_kwmap(fname="Pphi-kwmap", size="small", horiz=True):
-    Pphi = cmb.Pphi
-    scPphi = cmb.k[52]**3/(2*np.pi**2) * Pphi[:,52]
+def pphi_kwmap(fname="Pphi-kwmap", size="small", horiz=True, m=cmbmsq):
+    Pphi = m.Pphi
+    scPphi = m.k[52]**3/(2*np.pi**2) * Pphi[:,52]
     fig = P.figure()
     set_size(fig, size)
-    P.semilogy(cmb.tresult[-1]-cmb.tresult[865:], np.abs(scPphi[865:]))
+    P.semilogy(m.tresult[-1]-m.tresult[865:], np.abs(scPphi[865:]))
     ax = P.gca()
     if horiz:
-        ax.axvline(cmb.tresult[-1] - 21.24, ls="--", color="red")
+        ax.axvline(m.tresult[-1] - 21.24, ls="--", color="red")
     cg.reversexaxis()
     #Labels
     P.xlabel(calN)
@@ -158,7 +161,7 @@ def pphi_kwmap(fname="Pphi-kwmap", size="small", horiz=True):
     P.draw()
     return fig, fname
     
-def src_kwmap_3ranges(fname="src-kwmap-3ranges", size="small"):
+def src_kwmap_3ranges(fname="src-kwmap-3ranges", size="small", m=cmbmsq, fo=fomsq):
     fig = P.figure()
     set_size(fig, size)
     
@@ -169,7 +172,7 @@ def src_kwmap_3ranges(fname="src-kwmap-3ranges", size="small"):
     s2=abs(fo2.source[::2,17])
     s3=abs(fo3.source[::2,5])
 
-    tr = cmb.tresult[-1] - cmb.tresult[865:]
+    tr = m.tresult[-1] - m.tresult[865:]
     P.semilogy(tr, s3[865:])
     P.semilogy(tr, s2[865:])
     P.semilogy(tr, s1[865:])
@@ -186,16 +189,16 @@ def src_kwmap_3ranges(fname="src-kwmap-3ranges", size="small"):
     P.draw()
     return fig, fname
 
-def src_3ks(fname="src-3ks", size="small"):
+def src_3ks(fname="src-3ks", size="small", m=cmbmsq, fo=fomsq):
     #Initialise variables
-    tr0 = cmb.tresult[-1] - cmb.tresult[631:]
-    tr52 = cmb.tresult[-1] - cmb.tresult[865:]
-    tr1024 = cmb.tresult[-1] - cmb.tresult[1015:]
+    tr0 = m.tresult[-1] - m.tresult[631:]
+    tr52 = m.tresult[-1] - m.tresult[865:]
+    tr1024 = m.tresult[-1] - m.tresult[1015:]
     s0 = np.abs(fo.source[::2,0])
     s52 = np.abs(fo.source[::2,52])
     s1024 = np.abs(fo.source[::2,1024])
     #Setup legend
-    l = helpers.klegend(cmb.k[:][[0,52,1024]])
+    l = helpers.klegend(m.k[:][[0,52,1024]])
     #Setup figure and specify size
     fig = P.figure()
     set_size(fig, size)
@@ -323,6 +326,37 @@ def src_mvl(fname="src-mvl", size="small", zoom=False):
     P.draw()
     return fig, fname
     
+def compare_src(fname="compare_src", size="large", models=None, kix=52):
+    if models is None:
+        msq = c.make_wrapper_model("./foandsrc-FOCanonicalTwoStage-msqphisq-1.5e-61-6.153e-58-3e-61-195554.hf5")
+        lph = c.make_wrapper_model("./foandsrc-FOCanonicalTwoStage-lambdaphi4-1.5e-61-6.153e-58-3e-61-113146.hf5")
+        models = [msq, lph]
+    ts = [m.tresult for m in models]
+    srcs = [np.abs(m.source[:,kix]) for m in models]
+    #Setup figure
+    fig = P.figure()
+    set_size(fig, size)
+    lines = [P.semilogy(t[-1] - t, s) for t,s in zip(ts, srcs)]
+    cg.reversexaxis()
+    P.xlabel(calN)
+    P.ylabel(r"$|S|$")
+    ax = fig.gca()
+    ax.set_yticks(ax.get_yticks()[::2])
+    P.draw()
+    return fig, fname
+    
+def comp_src_msq_phi23(fname="comp_src_msq_phi23", size="large"):
+    msq =c.make_wrapper_model(os.path.join(resdir, "foandsrc-FOCanonicalTwoStage-msqphisq-5e-62-2.051e-58-1e-61-195554.hf5"))
+    phi23 = c.make_wrapper_model(os.path.join(resdir, "foandsrc-FOCanonicalTwoStage-phi2over3-5e-62-2.051e-58-1e-61-120949.hf5"))
+    models = [msq, phi23]
+    fig, fname = compare_src(fname, size, models)
+    ax = fig.gca()
+    ax.lines[1].set_linestyle("--")
+    ax.legend([r"$V(\varphi) = \frac{1}{2}m^2\varphi^2$", r"$V(\varphi) = \lambda\varphi^{\frac{2}{3}}$"], prop=prop, loc=0)
+    ax.set_xlim((71,-1))
+    P.draw()
+    return fig, fname
+    
 def phi2over3_params(fname="phi2over3_params", size="large"):
     filename = "/home/network/ith/results/param-search/phi2over3-3.5e-10-4e-10.hf5"
     try:
@@ -377,7 +411,7 @@ def lambdaphi4_params(fname="lambdaphi4_params", size="large"):
     ax.set_ylim((2.35e-9,2.55e-9))
     ax.axhline(2.457e-9, ls="--", color="black")
     ax.ticklabel_format(style="sci", scilimits=(0,0))
-    P.xlabel(r"$m$")
+    P.xlabel(r"$\lambda$")
     P.ylabel(r"$\mathcal{P}_\mathcal{R} (k_\mathrm{WMAP})$")
     P.draw()
     return fig, fname
@@ -414,4 +448,40 @@ def lambdaphi4_potential(fname="lambdaphi4_potential", size="large"):
     P.xlabel(r"$\varphi$")
     P.draw()
     return fig, fname
-    
+
+def hybrid2and4_params(fname="hybrid2and4_params", size="large"):
+    filename = "/home/network/ith/results/param-search/hybrid2and4-params.hf5"
+    try:
+        rf = tables.openFile(filename, "r")
+        pr = rf.root.params_results[:]
+    finally:
+        rf.close()
+    fig = P.figure()
+    set_size(fig, size)
+    P.plot(pr[:,1],pr[:,2], color="black")
+    ax = P.gca()
+    ax.set_xlim((1.49e-13,1.61e-13))
+    ax.set_ylim((2.35e-9,2.55e-9))
+    ax.axhline(2.457e-9, ls="--", color="black")
+    ax.ticklabel_format(style="sci", scilimits=(0,0))
+    P.xlabel(r"$\lambda$")
+    P.ylabel(r"$\mathcal{P}_\mathcal{R} (k_\mathrm{WMAP})$")
+    P.draw()
+    return fig, fname
+
+def hybrid2and4_potential(fname="hybrid2and4_potential", size="large"):
+    lph = c.make_wrapper_model(os.path.join(resdir, "cmb-lambdaphi4-5e-62-1.0245e-58-1e-61.hf5"))
+    fig = P.figure()
+    set_size(fig, size)
+    if size=="small":
+        fig.subplots_adjust(top=0.93)
+    yl = lph.bgmodel.yresult
+    vl = np.array([lph.potentials(y) for y in yl])
+    P.plot(yl[:,0], vl[:,0])
+    ax=P.gca()
+    ax.set_ylim((-0.5e-9, 16e-9))
+    ax.set_xlim((26,-1))
+    P.ylabel(r"$V(\varphi)$")
+    P.xlabel(r"$\varphi$")
+    P.draw()
+    return fig, fname
