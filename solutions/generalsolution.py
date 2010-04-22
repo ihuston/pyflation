9d3da15a1bc4a264f75999ff362054f485b044bc
Ian Huston <ian@ianhuston.net>
2010-04-22 18:12:39 +0100

    Find ks in __init__ and store.

--- solutions/generalsolution.py
+++ solutions/generalsolution.py
@@ -6,25 +6,29 @@ Created on 22 Apr 2010
 @author: Ian Huston
 '''
 
+import numpy as np
+
 class GeneralSolution(object):
     """General solution base class."""
     
-    def __init__(self):
+    def __init__(self, fixture):
         """Create a GeneralSolution object."""
-        pass
+        self.fixture = fixture
+        self.fullk = np.arange(fixture["kmin"], fixture["fullkmax"]+fixture["deltak"], fixture["deltak"])
+        self.k = self.fullk[:fixture["numsoks"]]
     
     def full_source_integrated(self):
         pass
     
-    def J_A(self, k, alpha, C1, C2):
+    def J_A(self, C1, C2):
         pass
     
-    def J_B(self, k, alpha, C3, C4):
+    def J_B(self, C3, C4):
         pass
     
-    def J_C(self, k, alpha, beta, C5):
+    def J_C(self, C5):
         pass
     
-    def J_D(self, k, alpha, beta, C6, C7):
+    def J_D(self, C6, C7):
         pass
     
