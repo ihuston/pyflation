10c2a6c50cb8fddbca00abd5383340dafd37ab0b
Ian Huston <ian@ianhuston.net>
2010-06-29 17:23:39 +0100

    Added some parsing options for command line arguments.

--- newrun.py
+++ newrun.py
@@ -10,6 +10,7 @@ import helpers
 import logging
 import sys
 import time
+from optparse import OptionParser
 
 def create_run_directory(newrundir, codedir):
     """Create the run directory using `newdir` as directory name."""
@@ -27,6 +28,9 @@ def create_run_directory(newrundir, codedir):
     logdir = os.path.join(newrundir, configuration.LOGDIRNAME)
     qsublogsdir = os.path.join(newrundir, configuration.QSUBLOGSDIRNAME)
     qsubscriptsdir = os.path.join(newrundir, configuration.QSUBSCRIPTSDIRNAME)
+    #debug info
+    logging.debug("resultsdir=%s, logdir=%s, qsublogsdir=%s, qsubscriptsdir=%s",
+                  resultsdir, logdir, qsublogsdir, qsubscriptsdir)
     
     try:
         os.makedirs(resultsdir)
@@ -44,22 +48,65 @@ def create_run_directory(newrundir, codedir):
     except ImportError:
         bzr_available = False
         
+    logging.debug("bzr_available=%s", bzr_available)
+    
     if bzr_available:
         accelerator_tree, source = BzrDir.open_tree_or_branch(codedir)
-        source.create_checkout(os.path.join(newrundir, configuration.CODEDIRNAME), None, True, accelerator_tree)
+        source.create_checkout(os.path.join(newrundir, configuration.CODEDIRNAME), 
+                               None, True, accelerator_tree)
+        
     else:
-        pass
+        raise NotImplementedError("Bazaar is needed to copy code directory. Please do this manually.")
     
     return
     
-if __name__ == '__main__':
-    logging.basicConfig()
-    newdir = os.path.join(os.getcwd(), time.strftime("%Y%m%d%H%M%S"))
-    print newdir
+def main():
+    """Check command line options and start directory creation."""
+    
+    #Parse command line options
+    parser = OptionParser()
+    parser.add_option("-n", "--newdir", dest="newdir",
+                  help="create run directory in NEWDIR", metavar="NEWDIR")
+    parser.add_option("-c", "--codedir", dest="codedir",
+                  help="copy code from CODEDIR", metavar="CODEDIR")
+    parser.add_option("-q", "--quiet",
+                  action="store_const", const=logging.FATAL, dest="loglevel", 
+                  help="only print fatal error messages")
+    parser.add_option("-v", "--verbose",
+                  action="store_const", const=logging.INFO, dest="loglevel", 
+                  help="print informative messages")
+    parser.add_option("--debug",
+                  action="store_const", const=logging.DEBUG, dest="loglevel", 
+                  help="print lots of debugging information")
+        
+    (options, args) = parser.parse_args()
+    
+    logging.basicConfig(level=options.loglevel)
+        
+    if options.newdir:
+        newdir = options.newdir
+        logging.debug("Option newdir specified with value %s.", options.newdir)
+    else:
+        newdir = os.path.join(os.getcwd(), time.strftime("%Y%m%d%H%M%S"))
+        logging.debug("Variable newdir created with value %s", newdir)
+    
+    if options.codedir:
+        codedir = options.codedir
+        logging.debug("Option codedir specified with value %s.", options.codedir)
+    else:
+        codedir = os.path.join(configuration.BASEDIR, 
+                               configuration.CODEDIRNAME, configuration.BZRBRANCH)
+        logging.debug("Variable codedir created with value %s.", codedir)
+        
     try:
-        create_run_directory(newdir)
+        create_run_directory(newdir, codedir)
     except Exception, e:
         logging.critical("Something went wrong! Quitting.")
         sys.exit(e)
     
     logging.info("New run directory created successfully.")
+    return
+
+if __name__ == '__main__':
+    main()
+    
