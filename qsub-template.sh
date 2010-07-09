f050eddba6c6022dd9f342a41c065e2ee9eafdad
Ian Huston <ian@ianhuston.net>
2010-07-09 12:10:43 +0100

    Add additional if statement to remove var="undefined".

--- qsub-template.sh
+++ qsub-template.sh
@@ -9,10 +9,11 @@
 
 echo -----------------------------------------
 echo Start: host `hostname`, date `date`
-if [ -n "$SGE_TASK_ID" ]
-    then
+if [ -n "$SGE_TASK_ID" ]; then
+    if [ "$SGE_TASK_ID" != "undefined" ]; then
         echo Task array: $SGE_TASK_FIRST-$SGE_TASK_LAST, step=$SGE_TASK_STEPSIZE
         echo My task-id:$SGE_TASK_ID
+    fi
 fi
 
 CODEDIR=%(codedir)s
