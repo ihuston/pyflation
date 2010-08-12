ebc5d77e045e15623f33c2fd4187f1c48a098560
Ian Huston <ian@ianhuston.net>
2010-08-12 16:22:12 +0100

    Added old src equations class to check if new equations have changed results.

--- sourceterm/srcequations.py
+++ sourceterm/srcequations.py
@@ -80,7 +80,7 @@ class SlowRollSource(SourceEquations):
         C1k = C1[..., np.newaxis]
         C2k = C2[..., np.newaxis]
         aterm = (C1k*q**2 + C2k*q**4) * dp1 * preaterm
-        J_A = romb(aterm, self.fixture["deltak"])
+        J_A = romb(aterm, self.deltak)
         return J_A
     
     def J_B(self, prebterm, dp1, C3, C4):
@@ -90,7 +90,7 @@ class SlowRollSource(SourceEquations):
         C3k = C3[..., np.newaxis]
         C4k = C4[..., np.newaxis]
         bterm = (C3k*q**3 + C4k*q**5) * dp1 * prebterm
-        J_B = romb(bterm, self.fixture["deltak"])
+        J_B = romb(bterm, self.deltak)
         return J_B
     
     def J_C(self, precterm, dp1dot, C5):
@@ -99,7 +99,7 @@ class SlowRollSource(SourceEquations):
         q = self.k
         C5k = C5[..., np.newaxis]
         cterm = (C5k*q**2) * dp1dot * precterm
-        J_C = romb(cterm, self.fixture["deltak"])
+        J_C = romb(cterm, self.deltak)
         return J_C
     
     def J_D(self, predterm, dp1dot, C6, C7):
@@ -109,7 +109,7 @@ class SlowRollSource(SourceEquations):
         C6k = C6[..., np.newaxis]
         C7k = C7[..., np.newaxis]
         dterm = (C6k*q + C7k*q**3) * dp1dot * predterm
-        J_D = romb(dterm, self.fixture["deltak"])
+        J_D = romb(dterm, self.deltak)
         return J_D
     
     def getthetaterms(self, dp1, dp1dot):
@@ -228,7 +228,7 @@ class SlowRollSource(SourceEquations):
 
 class FullSingleFieldSource(SourceEquations):
     """
-    Slow roll source term equations
+    Full single field (non slow-roll) source term equations
     """
     
     def __init__(self, *args, **kwargs):
@@ -242,7 +242,7 @@ class FullSingleFieldSource(SourceEquations):
         C1k = C1[..., np.newaxis]
         C2k = C2[..., np.newaxis]
         aterm = (C1k*q**2 + C2k*q**2*q**2) * dp1 * preaterm
-        J_A = romb(aterm, self.fixture["deltak"])
+        J_A = romb(aterm, self.deltak)
         return J_A
     
     def J_A2(self, preaterm, dpdot1, C17, C18):
@@ -252,7 +252,7 @@ class FullSingleFieldSource(SourceEquations):
         C17k = C17[..., np.newaxis]
         C18k = C18[..., np.newaxis]
         aterm = (C17k*q**2 + C18k*q**2*q**2) * dpdot1 * preaterm
-        J_A2 = romb(aterm, self.fixture["deltak"])
+        J_A2 = romb(aterm, self.deltak)
         return J_A2
     
     def J_B1(self, prebterm, dp1, C3, C4):
@@ -262,7 +262,7 @@ class FullSingleFieldSource(SourceEquations):
         C3k = C3[..., np.newaxis]
         C4k = C4[..., np.newaxis]
         bterm = (C3k*q**2*q + C4k*q**2*q**2*q) * dp1 * prebterm
-        J_B1 = romb(bterm, self.fixture["deltak"])
+        J_B1 = romb(bterm, self.deltak)
         return J_B1
     
     def J_B2(self, prebterm, dpdot1, C19):
@@ -271,7 +271,7 @@ class FullSingleFieldSource(SourceEquations):
         q = self.k
         C19k = C19[..., np.newaxis]
         bterm = (C19k*q**2*q) * dpdot1 * prebterm
-        J_B2 = romb(bterm, self.fixture["deltak"])
+        J_B2 = romb(bterm, self.deltak)
         return J_B2
     
     def J_C1(self, precterm, dp1dot, C5):
@@ -280,7 +280,7 @@ class FullSingleFieldSource(SourceEquations):
         q = self.k
         C5k = C5[..., np.newaxis]
         cterm = (C5k*q**2) * dp1dot * precterm
-        J_C = romb(cterm, self.fixture["deltak"])
+        J_C = romb(cterm, self.deltak)
         return J_C
     
     def J_C2(self, precterm, dp1, C20):
@@ -289,7 +289,7 @@ class FullSingleFieldSource(SourceEquations):
         q = self.k
         C20k = C20[..., np.newaxis]
         cterm = (C20k*q**2) * dp1 * precterm
-        J_C2 = romb(cterm, self.fixture["deltak"])
+        J_C2 = romb(cterm, self.deltak)
         return J_C2
     
     def J_D1(self, predterm, dp1dot, C6, C7):
@@ -299,7 +299,7 @@ class FullSingleFieldSource(SourceEquations):
         C6k = C6[..., np.newaxis]
         C7k = C7[..., np.newaxis]
         dterm = (C6k*q + C7k*q**2*q) * dp1dot * predterm
-        J_D = romb(dterm, self.fixture["deltak"])
+        J_D = romb(dterm, self.deltak)
         return J_D
     
     def J_D2(self, predterm, dp1, C21):
@@ -308,7 +308,7 @@ class FullSingleFieldSource(SourceEquations):
         q = self.k
         C21k = C21[..., np.newaxis]
         dterm = (C21k*q) * dp1 * predterm
-        J_D2 = romb(dterm, self.fixture["deltak"])
+        J_D2 = romb(dterm, self.deltak)
         return J_D2
     
     def J_E1(self, preeterm, dp1, C8, C9):
@@ -318,7 +318,7 @@ class FullSingleFieldSource(SourceEquations):
         C8k = C8[..., np.newaxis]
         C9k = C9[..., np.newaxis]
         eterm = (C8k*q**2 + C9k*q**2*q**2) * dp1 * preeterm
-        J_E1 = romb(eterm, self.fixture["deltak"])
+        J_E1 = romb(eterm, self.deltak)
         return J_E1
     
     def J_E2(self, preeterm, dp1dot, C10):
@@ -327,7 +327,7 @@ class FullSingleFieldSource(SourceEquations):
         q = self.k
         C10k = C10[..., np.newaxis]
         eterm = (C10k*q**2) * dp1dot * preeterm
-        J_E2 = romb(eterm, self.fixture["deltak"])
+        J_E2 = romb(eterm, self.deltak)
         return J_E2
     
     def J_F1(self, prefterm, dp1, C11, C12):
@@ -337,7 +337,7 @@ class FullSingleFieldSource(SourceEquations):
         C11k = C11[..., np.newaxis]
         C12k = C12[..., np.newaxis]
         fterm = (C11k*q**2 + C12k*q**2*q**2) * dp1 * prefterm
-        J_F1 = romb(fterm, self.fixture["deltak"])
+        J_F1 = romb(fterm, self.deltak)
         return J_F1
     
     def J_F2(self, prefterm, dpdot1, C13):
@@ -346,7 +346,7 @@ class FullSingleFieldSource(SourceEquations):
         q = self.k
         C13k = C13[..., np.newaxis]
         fterm = (C13k*q**2) * dpdot1 * prefterm
-        J_F2 = romb(fterm, self.fixture["deltak"])
+        J_F2 = romb(fterm, self.deltak)
         return J_F2
     
     def J_G1(self, pregterm, dp1, C14, C15):
@@ -356,7 +356,7 @@ class FullSingleFieldSource(SourceEquations):
         C14k = C14[..., np.newaxis]
         C15k = C15[..., np.newaxis]
         gterm = (C14k*q**2 + C15k*q**2*q**2) * dp1 * pregterm
-        J_G1 = romb(gterm, self.fixture["deltak"])
+        J_G1 = romb(gterm, self.deltak)
         return J_G1
     
     def J_G2(self, pregterm, dpdot1, C16):
@@ -365,7 +365,7 @@ class FullSingleFieldSource(SourceEquations):
         q = self.k
         C16k = C16[..., np.newaxis]
         gterm = (C16k*q**2) * dpdot1 * pregterm
-        J_G1 = romb(gterm, self.fixture["deltak"])
+        J_G1 = romb(gterm, self.deltak)
         return J_G1
     
     @profile
@@ -545,4 +545,121 @@ class FullSingleFieldSource(SourceEquations):
                                   + J_E1 + J_E2 + J_F1 + J_F2 + J_G1 + J_G2)
         return src
     
+class OldSlowRollSource(SourceEquations):
+    """
+    Using the old slow roll equations to solve the source term.
+    """
     
+    def __init__(self, *args, **kwargs):
+        """Class for slow roll source term equations"""
+        super(OldSlowRollSource, self).__init__(*args, **kwargs)
+        
+    def getthetaterms(self,dp1, dp1dot):
+        """Return array of integrated values for specified theta function and dphi function.
+        
+        Parameters
+        ----------
+        integrand_elements: tuple
+                Contains integrand arrays in order (k, q, theta)
+                 
+        dp1: array_like
+             Array of values for dphi1
+        
+        dp1dot: array_like
+                Array of values for dphi1dot
+                                      
+        Returns
+        -------
+        theta_terms: tuple
+                     Tuple of len(k)xlen(q) shaped arrays of integration results in form
+                     (\int(sin(theta) dp1(k-q) dtheta,
+                      \int(cos(theta)sin(theta) dp1(k-q) dtheta,
+                      \int(sin(theta) dp1dot(k-q) dtheta,
+                      \int(cos(theta)sin(theta) dp1dot(k-q) dtheta)
+                     
+        """
+    
+        sinth = np.sin(self.theta)
+        cossinth = np.cos(self.theta)*np.sin(self.theta)
+        theta_terms = np.empty([4, self.k.shape[0], self.k.shape[0]])
+        lenq = len(self.k)
+        
+        for n in xrange(len(self.k)):
+            #klq = klessq(onek, q, theta)
+            dphi_res = srccython.interpdps2(dp1, dp1dot, self.kmin, self.deltak, n, self.theta, lenq)
+            
+            theta_terms[0,n] = romb(sinth*dphi_res[0], dx=self.dtheta)
+            theta_terms[1,n] = romb(cossinth*dphi_res[0], dx=self.dtheta)
+            theta_terms[2,n] = romb(sinth*dphi_res[1], dx=self.dtheta)
+            theta_terms[3,n] = romb(cossinth*dphi_res[1], dx=self.dtheta)
+        return theta_terms
+
+
+        
+    def sourceterm(self, bgvars, a, potentials, dp1, dp1dot):
+        """Return unintegrated slow roll source term.
+        
+        The source term before integration is calculated here using the slow roll
+        approximation. This function follows the revised version of Eq (5.8) in 
+        Malik 06 (astro-ph/0610864v5).
+        
+        Parameters
+        ----------
+        bgvars: tuple
+                Tuple of background field values in the form `(phi, phidot, H)`
+        
+        a: float
+           Scale factor at the current timestep, `a = ainit*exp(n)`
+        
+        potentials: tuple
+                    Tuple of potential values in the form `(U, dU, dU2, dU3)`
+        
+        integrand_elements: tuple 
+             Contains integrand arrays in order (k, q, theta)
+                
+        dp1: array_like
+             Array of known dp1 values
+                 
+        dp1dot: array_like
+                Array of dpdot1 values
+                 
+        theta_terms: array_like
+                     3-d array of integrated theta terms of shape (4, len(k), len(q))
+                 
+        Returns
+        -------
+        src_integrand: array_like
+            Array containing the unintegrated source terms for all k and q modes.
+            
+        References
+        ----------
+        Malik, K. 2006, JCAP03(2007)004, astro-ph/0610864v5
+        """
+        #Unpack variables
+        phi, phidot, H = bgvars
+        U, dU, dU2, dU3 = potentials
+        k = self.k[...,np.newaxis]
+        q = self.k[np.newaxis, ...]
+        #Calculate dphi(q) and dphi(k-q)
+        dp1_q = dp1[:q.shape[-1]]
+        dp1dot_q = dp1dot[:q.shape[-1]]
+        aterm, bterm, cterm, dterm = self.getthetaterms(dp1, dp1dot)
+        
+        #Calculate unintegrated source term
+        #First major term:
+        if dU3!=0.0:
+            src_integrand = ((1/H**2) * dU3 * q**2 * dp1_q * aterm)
+        else:
+            src_integrand = np.zeros_like(aterm)
+        #Second major term:
+        src_integrand2 = (phidot/((a*H)**2)) * ((3*dU2*(a*q)**2 + 3.5*q**4 + 2*(k**2)*(q**2))*aterm 
+                          - (4.5 + (q/k)**2)* k * (q**3) * bterm) * dp1_q
+        #Third major term:
+        src_integrand3 = (phidot * ((-1.5*q**2)*cterm + (2 - (q/k)**2)*k*q*dterm) * dp1dot_q)
+        #Multiply by prefactor
+        src_integrand = src_integrand + src_integrand2 + src_integrand3
+        src_integrand *= (1/(2*np.pi)**2)
+        
+        src = romb(src_integrand, self.deltak)
+      
+        return src
