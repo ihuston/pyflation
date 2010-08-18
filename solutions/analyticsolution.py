e510dd95fc819a1896f0190b2a7d9b8e18e466a4
Ian Huston <ian@ianhuston.net>
2010-08-18 18:18:52 +0100

    Change C again to find factor of 2 error in imaginary part.

--- solutions/analyticsolution.py
+++ solutions/analyticsolution.py
@@ -98,35 +98,68 @@ class NoPhaseBunchDaviesSolution(AnalyticSolution):
         kmax = k[-1]
         kmin = k[0]
         
-        J_C = ((alpha**2*C5*(-(Sqrt(2)*k**3*
-              (-10000*beta**2 - (0+15360*1j)*beta*k + 6363*k**2)) - 
-           Sqrt(kmax*(-k + kmax))*
-            ((0+3840*1j)*beta*(k - kmax)**2*kmax + 
-              400*beta**2*(3*k**2 - 14*k*kmax + 8*kmax**2) + 
-              9*(15*k**4 + 10*k**3*kmax - 248*k**2*kmax**2 + 336*k*kmax**3 - 
-                 128*kmax**4)) + Sqrt(kmax*(k + kmax))*
-            ((0+3840*1j)*beta*kmax*(k + kmax)**2 + 
-              400*beta**2*(3*k**2 + 14*k*kmax + 8*kmax**2) - 
-              9*(-15*k**4 + 10*k**3*kmax + 248*k**2*kmax**2 + 336*k*kmax**3 + 
-                 128*kmax**4)) + Sqrt((k - kmin)*kmin)*
-            (-400*beta**2*(3*k**2 - 14*k*kmin + 8*kmin**2) - 
-              (0+60*1j)*beta*(15*k**3 - 54*k**2*kmin + 8*k*kmin**2 + 16*kmin**3) + 
-              9*(15*k**4 + 10*k**3*kmin - 248*k**2*kmin**2 + 336*k*kmin**3 - 
-                 128*kmin**4)) + Sqrt(kmin)*Sqrt(k + kmin)*
-            ((0+-3840*1j)*beta*kmin*(k + kmin)**2 - 
-              400*beta**2*(3*k**2 + 14*k*kmin + 8*kmin**2) + 
-              9*(-15*k**4 + 10*k**3*kmin + 248*k**2*kmin**2 + 336*k*kmin**3 + 
-                 128*kmin**4)) + (15*k**3*(-80*beta**2 - (0+60*1j)*beta*k + 9*k**2)*Pi)/
-            2. + 15*k**3*(80*beta**2 + (0+60*1j)*beta*k - 9*k**2)*
-            ArcTan(Sqrt(kmin/(k - kmin))) - 
-           15*k**3*(80*beta**2 + 9*k**2)*Log(2*(1 + Sqrt(2))*Sqrt(k)) + 
-           k**3*(Sqrt(2)*(-10000*beta**2 - (0+15360*1j)*beta*k + 6363*k**2) + 
-              15*(80*beta**2 + 9*k**2)*Log(2*Sqrt(k)) + 
-              15*(80*beta**2 + 9*k**2)*Log(2*(1 + Sqrt(2))*Sqrt(k))) - 
-           15*k**3*(80*beta**2 + 9*k**2)*Log(2*(Sqrt(kmax) + Sqrt(-k + kmax))) - 
-           15*k**3*(80*beta**2 + 9*k**2)*Log(2*(Sqrt(kmax) + Sqrt(k + kmax))) + 
-           15*k**3*(80*beta**2 + 9*k**2)*Log(2*(Sqrt(kmin) + Sqrt(k + kmin)))))/
-       (14400.*beta**2*k))
+        J_C = (-(alpha**2*C5*(Sqrt(2)*k*
+              (-10000*beta**2*k**2 - (0+15360*1j)*beta*k**3 + 
+                6363*k**4) + 
+             (15*k**3*(80*beta**2 + (0+60*1j)*beta*k - 9*k**2)*
+                Pi)/2. + 
+             15*k**3*(80*beta**2 + 9*k**2)*
+              Log(2*(Sqrt(k) + Sqrt(2)*Sqrt(k)))))/
+        (14400.*beta**2*k) + 
+       (2*alpha**2*C5*(Sqrt(2)*Sqrt(k)*
+             ((3*k**4.5)/(5.*beta) + 
+               (k**3.5*((0+-80*1j)*beta + 63*k))/(40.*beta) - 
+               (k**2.5*(400*beta**2 + (0+960*1j)*beta*k - 
+                    279*k**2))/(240.*beta) - 
+               (k**2.5*(560*beta**2 + (0+384*1j)*beta*k - 
+                    9*k**2))/(192.*beta) - 
+               (k**2.5*(80*beta**2 + 9*k**2))/(128.*beta)) + 
+            (k**3*(80*beta**2 + 9*k**2)*
+               Log(2*(Sqrt(k) + Sqrt(2)*Sqrt(k))))/(128.*beta)
+              + (k**3*(80*beta**2 + 9*k**2)*Log(2*Sqrt(k)))/
+             (128.*beta)))/(15.*beta*k) - 
+       (2*alpha**2*C5*(Sqrt(-k + kmax)*
+             ((k**2*(80*beta**2 + 9*k**2)*Sqrt(kmax))/
+                (128.*beta) - 
+               (k*(560*beta**2 - (0+384*1j)*beta*k - 9*k**2)*
+                  kmax**1.5)/(192.*beta) + 
+               ((400*beta**2 - (0+960*1j)*beta*k - 279*k**2)*
+                  kmax**2.5)/(240.*beta) + 
+               (((0+80*1j)*beta + 63*k)*kmax**3.5)/(40.*beta) - 
+               (3*kmax**4.5)/(5.*beta)) + 
+            Sqrt(k + kmax)*
+             (-(k**2*(80*beta**2 + 9*k**2)*Sqrt(kmax))/
+                (128.*beta) - 
+               (k*(560*beta**2 + (0+384*1j)*beta*k - 9*k**2)*
+                  kmax**1.5)/(192.*beta) - 
+               ((400*beta**2 + (0+960*1j)*beta*k - 279*k**2)*
+                  kmax**2.5)/(240.*beta) + 
+               (((0+-80*1j)*beta + 63*k)*kmax**3.5)/(40.*beta) + 
+               (3*kmax**4.5)/(5.*beta)) + 
+            (k**3*(80*beta**2 + 9*k**2)*
+               Log(2*(Sqrt(kmax) + Sqrt(-k + kmax))))/
+             (128.*beta) + 
+            (k**3*(80*beta**2 + 9*k**2)*
+               Log(2*(Sqrt(kmax) + Sqrt(k + kmax))))/
+             (128.*beta)))/(15.*beta*k) + 
+       (alpha**2*C5*(Sqrt(k - kmin)*Sqrt(kmin)*
+             (-400*beta**2*(3*k**2 - 14*k*kmin + 8*kmin**2) - 
+               (0+60*1j)*beta*
+                (15*k**3 - 54*k**2*kmin + 8*k*kmin**2 + 
+                  16*kmin**3) + 
+               9*(15*k**4 + 10*k**3*kmin - 248*k**2*kmin**2 + 
+                  336*k*kmin**3 - 128*kmin**4)) + 
+            Sqrt(kmin)*Sqrt(k + kmin)*
+             ((0+-3840*1j)*beta*kmin*(k + kmin)**2 - 
+               400*beta**2*(3*k**2 + 14*k*kmin + 8*kmin**2) + 
+               9*(-15*k**4 + 10*k**3*kmin + 
+                  248*k**2*kmin**2 + 336*k*kmin**3 + 
+                  128*kmin**4)) + 
+            15*k**3*(80*beta**2 + (0+60*1j)*beta*k - 9*k**2)*
+             ArcTan(Sqrt(kmin)/Sqrt(k - kmin)) + 
+            15*k**3*(80*beta**2 + 9*k**2)*
+             Log(2*(Sqrt(kmin) + Sqrt(k + kmin)))))/
+        (14400.*beta**2*k))
         return J_C
 
     def J_D(self, k, alpha, beta, C6, C7):
