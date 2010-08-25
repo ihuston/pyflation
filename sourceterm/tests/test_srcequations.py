3a662b5820ea90c0cd63116a610ede25558c5562
Ian Huston <ian@ianhuston.net>
2010-08-25 12:23:55 +0100

    Add tests directory to sourceterm package and start test module.

---
+++ sourceterm/tests/test_srcequations.py
@@ -0,0 +1,18 @@
+'''
+Created on 25 Aug 2010
+
+@author: ith
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
