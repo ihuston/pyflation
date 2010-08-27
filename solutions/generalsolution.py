a7e22ba37a529ef8997cf252a715abd8dffaf763
Ian Huston <ian@ianhuston.net>
2010-08-27 15:33:25 +0100

    Remove unused functions from general solution.

--- solutions/generalsolution.py
+++ solutions/generalsolution.py
@@ -18,15 +18,3 @@ class GeneralSolution(object):
     def full_source_from_model(self, m, nix):
         pass
     
-    def J_A(self, C1, C2):
-        pass
-    
-    def J_B(self, C3, C4):
-        pass
-    
-    def J_C(self, C5):
-        pass
-    
-    def J_D(self, C6, C7):
-        pass
-    
