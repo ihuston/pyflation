073faa2d6e5fca8cad9e1317498b3b36a8d2716b
Ian Huston <ian@ianhuston.net>
2010-10-22 17:49:28 +0100

    Test start of array ystart.

--- tests/test_saveallresults.py
+++ tests/test_saveallresults.py
@@ -8,7 +8,7 @@ import numpy as np
 import sys
 
 def main():
-    ystart = np.zeros((4,))
+    ystart = np.zeros((4,1))
     callingparams = {
               'datetime': '20101022173354',
               'dxsav': 0.0,
