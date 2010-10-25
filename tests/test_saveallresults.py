a9c5f4166ad011656e986f2b81e8779f91e8a499
Ian Huston <ian@ianhuston.net>
2010-10-25 14:56:03 +0100

    Change ystart in test.

--- tests/test_saveallresults.py
+++ tests/test_saveallresults.py
@@ -10,29 +10,10 @@ import sys
 def main():
     ystart = np.zeros((4,1000))
     callingparams = {
-              'datetime': '20101022173354',
-              'dxsav': 0.0,
-              'eps': 1e-10,
-              'potential_func': 'msqphisq',
-              'solver': 'rkdriver_new',
-              'tend': 81.640000000009962,
-              'tstart': 0.0,
-              'tstep_min': 0.00020000000000000001,
-              'tstep_wanted': 0.02,
               'ystart': ystart}
     
     hf5dict = {
-        "solver" : tables.StringCol(50),
-        "ystart" : tables.Float64Col(ystart.shape),
-        "tstart" : tables.Float64Col(),
-        "simtstart" : tables.Float64Col(),
-        "potential_func" : tables.StringCol(255),
-        "tend" : tables.Float64Col(),
-        "tstep_wanted" : tables.Float64Col(),
-        "tstep_min" : tables.Float64Col(),
-        "eps" : tables.Float64Col(),
-        "dxsav" : tables.Float64Col(),
-        "datetime" : tables.Float64Col()
+        "ystart" : tables.Float64Col(ystart.shape)
         }
     
     rf = tables.openFile("./test.hf5", "w")
