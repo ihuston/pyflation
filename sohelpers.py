db91cd0b1a5a9a3e2b08265ee7246ec31154d845
Ian Huston <ian@ianhuston.net>
2010-10-29 11:41:11 +0100

    Remove combine function to use in combine.py.

--- sohelpers.py
+++ sohelpers.py
@@ -84,75 +84,7 @@ def combine_source_and_fofile(sourcefile, fofile, newfile=None):
         nf.close()
     return newfile
         
-def combine_results(fofile, sofile, newfile=None):
-    """Combine the first and second order results from given files, and save in newfile."""
-    if not newfile:
-        now = time.strftime("%Y%m%d%H%M")
-        newfile = c.RESULTS_PATH + "cmb" + now + ".hf5"
-        _log.info("Filename set to " + newfile)
-        
-    if os.path.isdir(os.path.dirname(newfile)):
-        if os.path.isfile(newfile):
-            raise IOError("File already exists!")
-        else:
-            _log.debug("File does not exist, using write mode.")
-            filemode = "w" #Writing to new file
-    else:
-        raise IOError("Directory 'results' does not exist")
-    #Add compression
-    filters = tables.Filters(complevel=1, complib="zlib")
-    try:
-        sf = tables.openFile(sofile, "r")
-        ff = tables.openFile(fofile, "r")
-        nf = tables.openFile(newfile, filemode, filters=filters)
-    except IOError:
-        _log.exception("Error opening files!")
-        raise
-    try:
-        #Create groups required
-        comgrp = nf.createGroup(nf.root, "results", "Combined first and second order results")
-        _log.debug("Results group created in combined file.")
-        #Store bg results:
-        bggrp = nf.copyNode(ff.root.bgresults, nf.root, recursive=True)
-        _log.debug("Bg results copied.")
-        #Save results
-        oldyshape = list(sf.root.results.yresult[:,:,0:0].shape) #2nd order shape
-        oldyshape[1] += ff.root.results.yresult.shape[1] #add number of 1st order vars
-        yresarr = nf.createEArray(comgrp, "yresult", tables.Float64Atom(), oldyshape, filters=filters, chunkshape=(10,7,10))
-        _log.debug("New yresult array with shape %s created.", str(oldyshape))
-        #Copy other important arrays
-        karr = nf.copyNode(sf.root.results.k, comgrp)
-        foparams = nf.copyNode(ff.root.results.parameters, comgrp, newname="foparameters")
-        soparams = nf.copyNode(sf.root.results.parameters, comgrp, newname="soparameters")
-        #Copy parameters and change classname for compatibility
-        params = nf.copyNode(sf.root.results.parameters, comgrp, newname="parameters")
-        params.cols.classname[0] = "CombinedCanonicalFromFile"
-        params.flush()
-        tresarr = nf.copyNode(sf.root.results.tresult, comgrp)
-        _log.debug("K array, first and second order parameters copied.")
-        #Only copy foystart if it exists
-        if "foystart" in ff.root.results:
-            foystarr = nf.copyNode(ff.root.results.foystart, comgrp)
-            fotstarr = nf.copyNode(ff.root.results.fotstart, comgrp)
-            fotstixarr = nf.copyNode(ff.root.results.fotstartindex, comgrp)
-            _log.debug("foystart, fotstart, fotstartindex arrays copied.")
-        #Get results from first and second order
-        fyr = ff.root.results.yresult
-        syr = sf.root.results.yresult
-        #Begin main loop
-        _log.debug("Beginning main combination loop...")
-        for frow, srow in zip(fyr.iterrows(), syr.iterrows()):
-            nrow = np.concatenate((frow[:,::2], srow)).transpose()[..., np.newaxis]
-            yresarr.append(nrow)
-        _log.debug("Main combination loop finished.")
-        nf.flush()
-    finally:
-        sf.close()
-        ff.close()
-        nf.close()
-    #Successful execution
-    _log.debug("First and second order files successfully combined in %s.", newfile)
-    return newfile
+
    
 def soderivs_magnitude(m, **kwargs):
     """Equation of motion for second order perturbations including source term"""
