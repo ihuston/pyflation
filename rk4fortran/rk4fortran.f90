f6bc9940935b52c44a31f5e79cbf68d8109acd75
Ian Huston <ian@ianhuston.net>
2010-11-01 17:52:40 +0000

    Add h variable.

--- rk4fortran/rk4fortran.f90
+++ rk4fortran/rk4fortran.f90
@@ -1,14 +1,14 @@
-subroutine rk4stepks(x, y, out_vector, n,m,p, in_out_vec)
+subroutine rk4stepks(x, y, h, out_vector, n,m,p, in_out_vec)
 !   Do one step of the classical 4th order Runge Kutta method,
 !   starting from y at x with time step h and derivatives given by derivs
   implicit none
   integer :: n,m,p
-  real(8) :: x
+  real(8) :: x, h
   real(8) :: y(n,m)
   real(8) :: out_vector(n+m-1)
   real(8) :: in_out_vec(p)
 
-!f2py real(8), intent(in) :: x
+!f2py real(8), intent(in) :: x, h
 !f2py real(8), intent(out), dimension(n+m-1) :: out_matix
 !f2py real(8), intent(in), dimension(n,m) :: y
 !f2py real(8), intent(inout), dimension(p) :: in_out_vec
