f1d265a16ffa2b66b68b657b7f6e7630d669129a
Ian Huston <ian@ianhuston.net>
2010-11-01 17:41:35 +0000

    Add some more f2py statements to rk4fortran.

--- rk4fortran/rk4fortran.f90
+++ rk4fortran/rk4fortran.f90
@@ -14,6 +14,9 @@ subroutine rk4stepks(x, y, h, n, m, yout, derivs)
     real :: derivs
     real, dimension(0:n-1,0:m-1), intent(out) :: yout
     external derivs
+!f2py real, intent(in), dimension(0:n-1,0:m-1) :: y
+!f2py real, intent(out), dimension(0:n-1,0:m-1) :: yout
+!f2py real, intent(in) :: x, h
 !f2py depend(n,m) :: y, yt, dyt, dym, yout, derivs
 
     hh = h*0.5 !Half time step
