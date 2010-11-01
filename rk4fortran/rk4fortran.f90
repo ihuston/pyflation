ac6945c6f5a8e9b2dc74d2beb58b092a7d8b295d
Ian Huston <ian@ianhuston.net>
2010-11-01 17:49:05 +0000

    Start again with code from vec_in_out.

--- rk4fortran/rk4fortran.f90
+++ rk4fortran/rk4fortran.f90
@@ -1,52 +1,19 @@
 subroutine rk4stepks(x, h, n, m, yout, derivs)
 !   Do one step of the classical 4th order Runge Kutta method,
 !   starting from y at x with time step h and derivatives given by derivs
-    implicit none
+  implicit none
+  integer :: n,m,p
+  real(8) :: in_matrix(n,m)
+  real(8) :: out_vector(n+m-1)
+  real(8) :: in_out_vec(p)
 
-    real, intent(in) :: x
-    real, intent(in) :: h
-    integer :: n, m
-!    real, intent(in) :: y(0:n-1,0:m-1)
-
-
-    real :: hh, h6, xh
-    real, dimension(0:n-1,0:m-1) :: yt, dyt, dym, dydx
-    real :: derivs
-    real, dimension(0:n-1,0:m-1), intent(out) :: yout
-    external derivs
+!f2py real(8), intent(out), dimension(n+m-1) :: out_matix
+!f2py real(8), intent(in), dimension(n,m) :: in_matrix
+!f2py real(8), intent(inout), dimension(p) :: in_out_vec
+!f2py integer, intent(in) :: n,m, p
 
 
-!f2py real, intent(out), dimension(0:n-1,0:m-1) :: yout
-!f2py integer, intent(in) :: n, m
-!f2py real, intent(in) :: x, h
-!f2py depend(n,m) :: yt, dyt, dym, yout, derivs
-
-    hh = h*0.5 !Half time step
-    h6 = h/6.0 !Sixth of time step
-    xh = x + hh ! Halfway point in x direction
-
-!    dydx = derivs(y,x)
-
-    !First step, we already have derivatives from dydx
-    !yt = y + hh*dydx
-
-    !Second step, get new derivatives
-    !dyt = derivs(yt, xh)
-
-    !yt = y + hh*dyt
-
-    !Third step
-    !dym = derivs(yt, xh)
-
-    !yt = y + h*dym
-    !dym = dym + dyt
-
-    !Fourth step
-    !dyt = derivs(yt, x+h)
-
-    !Accumulate increments with proper weights
-    !yout = y + h6*(dydx + dyt + 2*dym)
-    yout = x
-
+  out_vector = (/ in_matrix(:,1), in_matrix(:,2) /)
+  in_out_vec = in_out_vec*5
     return
     end subroutine
