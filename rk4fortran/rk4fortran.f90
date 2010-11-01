a6266d709cc9989c0b0b79d49a1704d5fd81f573
Ian Huston <ian@ianhuston.net>
2010-11-01 18:02:05 +0000

    Comment out derivs to test where error is coming from.

--- rk4fortran/rk4fortran.f90
+++ rk4fortran/rk4fortran.f90
@@ -1,4 +1,4 @@
-subroutine rk4stepks(x, y, h, yout, n, m, derivs)
+subroutine rk4stepks(x, y, h, yout, n, m)
 !   Do one step of the classical 4th order Runge Kutta method,
 !   starting from y at x with time step h and derivatives given by derivs
   implicit none
@@ -6,14 +6,14 @@ subroutine rk4stepks(x, y, h, yout, n, m, derivs)
   real(8) :: x, h
   real(8) :: y(0:n-1,0:m-1)
   real(8) :: yout(0:n-1,0:m-1)
-  real(8) :: derivs
-  external derivs
+!  real(8) :: derivs
+!  external derivs
 
 !f2py real(8), intent(in) :: x, h
 !f2py real(8), intent(out), dimension(0:n-1,0:m-1) :: yout
 !f2py real(8), intent(in), dimension(0:n-1,0:m-1) :: y
 !f2py integer, intent(in) :: n,m
 
-  yout = derivs(y, x)
+  yout = y
     return
     end subroutine
