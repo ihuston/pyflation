dfed2c27c1380dd00529caf985606d7247d5bc62
Ian Huston <ian@ianhuston.net>
2010-11-01 16:52:48 +0000

    Commented out lines in rk4fortran to test.

--- rk4fortran/rk4fortran.f90
+++ rk4fortran/rk4fortran.f90
@@ -1,4 +1,4 @@
-subroutine rk4stepks(x, y, h, derivs, n, m, yout)
+subroutine rk4stepks(x, y, h, n, m, yout)
 !   Do one step of the classical 4th order Runge Kutta method,
 !   starting from y at x with time step h and derivatives given by derivs
     implicit none
@@ -8,7 +8,7 @@ subroutine rk4stepks(x, y, h, derivs, n, m, yout)
     real, intent(in) :: h
     integer, intent(in) :: n
     integer, intent(in) :: m
-    external derivs
+    !external derivs
 
     real :: hh, h6, xh
     real, dimension(0:n-1,0:m-1) :: yt, dyt, dym
@@ -20,23 +20,23 @@ subroutine rk4stepks(x, y, h, derivs, n, m, yout)
     xh = x + hh ! Halfway point in x direction
 
     !First step, we already have derivatives from dydx
-    yt = y + hh*dydx
+    !yt = y + hh*dydx
 
     !Second step, get new derivatives
-    dyt = derivs(yt, xh)
+    !dyt = derivs(yt, xh)
 
-    yt = y + hh*dyt
+    !yt = y + hh*dyt
 
     !Third step
-    dym = derivs(yt, xh)
+    !dym = derivs(yt, xh)
 
-    yt = y + h*dym
-    dym = dym + dyt
+    !yt = y + h*dym
+    !dym = dym + dyt
 
     !Fourth step
-    dyt = derivs(yt, x+h)
+    !dyt = derivs(yt, x+h)
 
     !Accumulate increments with proper weights
-    yout = y + h6*(dydx + dyt + 2*dym)
+    !yout = y + h6*(dydx + dyt + 2*dym)
 
     return
