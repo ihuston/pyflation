ff1460cea2146e4994c6a2a40fcc4e213ab1693b
Ian Huston <ian@ianhuston.net>
2010-11-01 17:55:22 +0000

    Change shape of out_vector.

--- rk4fortran/rk4fortran.f90
+++ rk4fortran/rk4fortran.f90
@@ -5,14 +5,14 @@ subroutine rk4stepks(x, y, h, out_vector, n,m)
   integer :: n,m
   real(8) :: x, h
   real(8) :: y(0:n-1,0:m-1)
-  real(8) :: out_vector(n+m-1)
+  real(8) :: out_vector(0:n-1,0:m-1)
 
 !f2py real(8), intent(in) :: x, h
-!f2py real(8), intent(out), dimension(n+m-1) :: out_matix
+!f2py real(8), intent(out), dimension(0:n-1,0:m-1) :: out_matix
 !f2py real(8), intent(in), dimension(0:n-1,0:m-1) :: y
 !f2py integer, intent(in) :: n,m
 
 
-  out_vector = (/ y(:,1), y(:,2) /)
+  out_vector = y
     return
     end subroutine
