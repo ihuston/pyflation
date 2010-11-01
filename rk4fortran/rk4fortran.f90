c8230224d746e98bcfbf3445de2d17caa6bb382f
Ian Huston <ian@ianhuston.net>
2010-11-01 17:58:00 +0000

    Add external function.

--- rk4fortran/rk4fortran.f90
+++ rk4fortran/rk4fortran.f90
@@ -1,4 +1,4 @@
-subroutine rk4stepks(x, y, h, yout, n,m)
+subroutine rk4stepks(x, y, h, yout, n, m, derivs)
 !   Do one step of the classical 4th order Runge Kutta method,
 !   starting from y at x with time step h and derivatives given by derivs
   implicit none
@@ -6,11 +6,13 @@ subroutine rk4stepks(x, y, h, yout, n,m)
   real(8) :: x, h
   real(8) :: y(0:n-1,0:m-1)
   real(8) :: yout(0:n-1,0:m-1)
+  real(8), external :: derivs(0:n-1, 0:m-1)
 
 !f2py real(8), intent(in) :: x, h
 !f2py real(8), intent(out), dimension(0:n-1,0:m-1) :: yout
 !f2py real(8), intent(in), dimension(0:n-1,0:m-1) :: y
 !f2py integer, intent(in) :: n,m
+!f2py real(8), external, dimension(0:n-1, 0:m-1) :: derivs
 
 
   yout = y
