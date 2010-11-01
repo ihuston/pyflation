48ddc6685e538ac2b4d82fd9caf7e56fd834b581
Ian Huston <ian@ianhuston.net>
2010-11-01 17:54:25 +0000

    Change to zero indexing for y.

--- rk4fortran/rk4fortran.f90
+++ rk4fortran/rk4fortran.f90
@@ -4,12 +4,12 @@ subroutine rk4stepks(x, y, h, out_vector, n,m)
   implicit none
   integer :: n,m
   real(8) :: x, h
-  real(8) :: y(n,m)
+  real(8) :: y(0:n-1,0:m-1)
   real(8) :: out_vector(n+m-1)
 
 !f2py real(8), intent(in) :: x, h
 !f2py real(8), intent(out), dimension(n+m-1) :: out_matix
-!f2py real(8), intent(in), dimension(n,m) :: y
+!f2py real(8), intent(in), dimension(0:n-1,0:m-1) :: y
 !f2py integer, intent(in) :: n,m
 
 
