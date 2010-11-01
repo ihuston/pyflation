8b567f7bb1eb83978f6d0326d4b374c2df0b2573
Ian Huston <ian@ianhuston.net>
2010-11-01 17:33:39 +0000

    Change dimensions of n.

--- rk4fortran/rk4fortran.f90
+++ rk4fortran/rk4fortran.f90
@@ -4,9 +4,9 @@ subroutine rk4stepks(x, y, h, n, m, yout, derivs)
     implicit none
 
     real, intent(in) :: x
-    real, intent(in) :: y
     real, intent(in) :: h
     integer :: n, m
+    real, dimension(0:n-1,0:m-1) intent(in) :: y
 
 
     real :: hh, h6, xh
