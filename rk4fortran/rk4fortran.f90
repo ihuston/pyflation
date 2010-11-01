658f9d8f1d701613712072dd7a18afb6b3b99935
Ian Huston <ian@ianhuston.net>
2010-11-01 17:56:08 +0000

    Change name of out_vector to yout.

--- rk4fortran/rk4fortran.f90
+++ rk4fortran/rk4fortran.f90
@@ -1,18 +1,18 @@
-subroutine rk4stepks(x, y, h, out_vector, n,m)
+subroutine rk4stepks(x, y, h, yout, n,m)
 !   Do one step of the classical 4th order Runge Kutta method,
 !   starting from y at x with time step h and derivatives given by derivs
   implicit none
   integer :: n,m
   real(8) :: x, h
   real(8) :: y(0:n-1,0:m-1)
-  real(8) :: out_vector(0:n-1,0:m-1)
+  real(8) :: yout(0:n-1,0:m-1)
 
 !f2py real(8), intent(in) :: x, h
-!f2py real(8), intent(out), dimension(0:n-1,0:m-1) :: out_matix
+!f2py real(8), intent(out), dimension(0:n-1,0:m-1) :: yout
 !f2py real(8), intent(in), dimension(0:n-1,0:m-1) :: y
 !f2py integer, intent(in) :: n,m
 
 
-  out_vector = y
+  yout = y
     return
     end subroutine
