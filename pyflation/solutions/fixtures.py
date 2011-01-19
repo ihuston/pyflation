061e5f77137e6bd18c43410f4309433486520f85
Ian Huston <ian@ianhuston.net>
2011-01-19 17:20:19 +0000

    Fix reference to getkend.

--- pyflation/solutions/fixtures.py
+++ pyflation/solutions/fixtures.py
@@ -6,7 +6,6 @@ Created on 22 Apr 2010
 @author: Ian Huston
 '''
 
-from pyflation.run_config import getkend
 from pyflation import helpers
 
 kmins_default = [1e-61, 3e-61, 1e-60]
@@ -22,7 +21,7 @@ def generate_fixtures(kmins=kmins_default, deltaks=deltaks_default, numsoks=nums
     """Generator for fixtures created from cartesian products of input lists."""
     c = helpers.cartesian_product([kmins, deltaks, numsoks, nthetas])
     for now in c:
-        fullkmax = getkend(now[0], now[1], now[2])
+        fullkmax = helpers.getkend(now[0], now[1], now[2])
         fx = {"kmin":now[0], "deltak":now[1], "numsoks":now[2], "fullkmax":fullkmax, 
               "nthetas":now[3]}
         yield fx
@@ -35,7 +34,7 @@ def fixture_from_model(m, numsoks=None, nthetas=nthetas_default[-1]):
     if not numsoks:
         numsoks = numsoks_default[-1]
         
-    fullkmax = getkend(m.k[0], m.k[1]-m.k[0], numsoks)
+    fullkmax = helpers.getkend(m.k[0], m.k[1]-m.k[0], numsoks)
     
     fx = {"kmin":m.k[0], "deltak":m.k[1]-m.k[0], "numsoks":numsoks, 
                "fullkmax":fullkmax, "nthetas":nthetas}
