e7938ae407fb8eda893397078948fbc27916dd82
Ian Huston <ian@ianhuston.net>
2011-01-19 15:32:24 +0000

    Update with name and author and script file names.

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from Cython.Distutils import build_ext
 import numpy
 from pyflation import configuration
 
+###############
+VERSION = '0.1.0'
+
+
 ext_modules = [Extension("pyflation.sourceterm.srccython", ["pyflation/sourceterm/srccython.pyx"], 
                extra_compile_args=["-g"], 
                extra_link_args=["-g"],
@@ -14,14 +18,22 @@ ext_modules = [Extension("pyflation.sourceterm.srccython", ["pyflation/sourceter
                extra_link_args=["-g"],
                include_dirs=[numpy.get_include()]),
                #
-               #Extension("cythontesting", ["cythontesting.pyx"], 
-               #extra_compile_args=["-g"], 
-               #extra_link_args=["-g"],
-               #include_dirs=[numpy.get_include()])
                ]
 
 
-setup_args = dict(name=configuration.PROGRAM_NAME,
+setup_args = dict(name='Pyflation',
+                  version=VERSION,
+                  author='Ian Huston',
+                  author_email='ian.huston@gmail.com',
+                  url='http://www.maths.qmul.ac.uk/~ith/pyflation',
+                  description='Cosmological Inflation in Python',
+                  packages=['pyflation', 'pyflation.sourceterm',
+                            'pyflation.solutions'],
+                  scripts=['bin/firstorder.py', 'bin/source.py', 
+                           'bin/secondorder.py', 'bin/combine.py',
+                           'bin/srcmerge.py', 'bin/start.py',
+                           'bin/newrun.py'],
+                  data_files=[('bin', ['bin/qsub-template.sh'])],
                   cmdclass = {'build_ext': build_ext},
                   ext_modules = ext_modules)
 
