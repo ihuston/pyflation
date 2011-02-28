588fb11014904610cac24a71d2078a06c569c0f0
Ian Huston <ian@ianhuston.net>
2011-02-28 16:56:46 +0000

    Remove self from arg list.

--- pyflation/cosmographs.py
+++ pyflation/cosmographs.py
@@ -128,6 +128,6 @@ class LogFormatterTeXExponent(P.LogFormatter, object):
         
     def __call__(self, *args, **kwargs):
         """Wrap call to parent class with change to tex notation."""
-        label = super(LogFormatterTeXExponent, self).__call__(self,  *args, **kwargs)
+        label = super(LogFormatterTeXExponent, self).__call__(*args, **kwargs)
         label = helpers.eto10(label)
         return label
