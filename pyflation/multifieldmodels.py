1d29256168abc263905996fc16f3cd95cadb31fe
Ian Huston <ian@ianhuston.net>
2011-04-07 13:41:48 +0100

    Added new multifield module to hold multifield classes.

---
+++ pyflation/multifieldmodels.py
@@ -0,0 +1,20 @@
+'''multifield.py - Classes for multi field cosmological models
+
+Author: Ian Huston
+For license and copyright information see LICENSE.txt which was distributed with this file.
+
+'''
+
+import cosmomodels as c
+
+class MultiFieldModels(c.CosmologicalModel):
+    '''
+    Parent class for all multifield models. 
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
