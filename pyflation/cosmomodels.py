59fa63b5700a2538e6564d1078591b52e1919087
Ian Huston <ian@ianhuston.net>
2011-09-01 15:52:46 +0100

    Change size of dydx to fit shape of fields.

--- pyflation/cosmomodels.py
+++ pyflation/cosmomodels.py
@@ -587,9 +587,9 @@ class CanonicalFirstOrder(PhiModels):
         
         #Set derivatives taking care of k type
         if type(k) is np.ndarray or type(k) is list: 
-            dydx = np.zeros((4*self.nfields + 1,len(k)), dtype=y.dtype)
+            dydx = np.zeros((2*self.nfields**2 + 2*self.nfields + 1,len(k)), dtype=y.dtype)
         else:
-            dydx = np.zeros(4*self.nfields + 1, dtype=y.dtype)
+            dydx = np.zeros(2*self.nfields**2 + 2*self.nfields + 1, dtype=y.dtype)
             
         #d\phi_0/dn = y_1
         dydx[self.phis_ix] = y[self.phidots_ix] 
