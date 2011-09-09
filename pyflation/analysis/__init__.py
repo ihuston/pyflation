5365ac3b978c982d4c5452265be07044a857187c
Ian Huston <ian@ianhuston.net>
2011-09-09 10:47:54 +0100

    Add new analysis package and start deltaprel module.

---
+++ pyflation/analysis/__init__.py
@@ -0,0 +1,5 @@
+""" analysis package - Provides modules to analyse results from cosmomodels runs.
+
+Author: Ian Huston
+For license and copyright information see LICENSE.txt which was distributed with this file.
+"""
---
+++ pyflation/analysis/deltaprel.py
@@ -0,0 +1,7 @@
+''' pyflation.analysis.deltaprel - Module to calculate relative pressure
+perturbations.
+
+Author: Ian Huston
+For license and copyright information see LICENSE.txt which was distributed with this file.
+
+'''
