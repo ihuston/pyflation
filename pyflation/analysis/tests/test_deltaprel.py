b57d4ee69e8d48325bb8e611e6f361937159e740
Ian Huston <ian@ianhuston.net>
2011-09-09 11:55:24 +0100

    Start tests directory.

---
+++ pyflation/analysis/tests/test_deltaprel.py
@@ -0,0 +1,34 @@
+''' test_deltaprel - Test functions for deltaprel module
+
+Author: Ian Huston
+For license and copyright information see LICENSE.txt which was distributed with this file.
+'''
+import numpy as np
+from numpy.testing import assert_, assert_raises
+
+from pyflation.analysis import deltaprel
+import nose
+
+
+
+
+class TestSoundSpeeds():
+    
+    def setup(self):
+        self.Vphi = np.arange(24).reshape((4,3,2))
+        self.phidot = self.Vphi
+        self.H = np.arange(8).reshape((4,1,2))
+    
+
+    def test_shape(self):
+        """Test whether the soundspeeds are shaped correctly."""    
+        arr = deltaprel.soundspeeds(self.Vphi, self.phidot, self.H)
+        assert_(arr.shape == self.Vphi.shape)
+        
+    def test_calc(self):
+        """Test results of calculation."""
+        arr = deltaprel.soundspeeds(self.Vphi, self.phidot, self.H)
+        
+    def test_wrongshape(self):
+        """Test that wrong shapes raise exception."""
+        
