4404f1e81d9be6d7493388ca98300abf916136a6
Ian Huston <ian@ianhuston.net>
2011-09-28 17:01:03 +0100

    Fix typo in the power of H.

--- pyflation/analysis/deltaprel.py
+++ pyflation/analysis/deltaprel.py
@@ -129,7 +129,7 @@ def deltarhosmatrix(Vphi, phidot, H, modes, modesdot, axis):
     #Add another dimension to internalsum result
     internalsum = np.expand_dims(internalsum, axis)
     
-    result = H*phidot*modesdot
+    result = H**2*phidot*modesdot
     result -= 0.5*H**3*phidot**2*internalsum
     result += Vphi*modes
     
