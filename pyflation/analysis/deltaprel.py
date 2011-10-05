f17916183cb7cb620b4cca645e4ae5e20ea86cd2
Ian Huston <ian@ianhuston.net>
2011-10-05 16:24:01 +0100

    Add components from model function and put checks on shape of variables into a separate function.

--- pyflation/analysis/deltaprel.py
+++ pyflation/analysis/deltaprel.py
@@ -69,6 +69,65 @@ def fullrhodot(phidot, H, axis=-1):
     """
     return np.sum(rhodots(phidot, H), axis=axis)
 
+def correct_shapes(Vphi, phidot, H, modes, modesdot, axis):
+    """Return variables with the correct shapes for calculation.
+    
+    Arguments
+    ---------
+    Vphi: array_like
+          First derivative of the potential with respect to the fields
+          
+    phidot: array_like
+            First derivative of the field values with respect to efold number N.
+            
+    H: array_like
+       The Hubble parameter
+       
+    modes: array_like
+           Mode matrix of first order perturbations. Component array should
+           have two dimensions of length nfields.
+           
+    modesdot: array_like
+           Mode matrix of N-derivative of first order perturbations. Component array should
+           have two dimensions of length nfields.
+           
+    axis: integer
+          Specifies which axis is first in mode matrix, e.g. if modes has shape
+          (100,3,3,10) with nfields=3, then axis=1. The two mode matrix axes are
+          assumed to be beside each other so (100,3,10,3) would not be valid.
+          
+    Returns
+    -------
+    
+    result: tuple
+            Tuple of the variables with correct shapes in the form
+            (Vphi, phidot, H, modes, modesdot, axis)
+           
+    """
+    mshape = modes.shape
+    if mshape[axis+1] != mshape[axis]:
+        raise ValueError("The mode matrix dimensions are not together.")
+    if mshape != modesdot.shape:
+        raise ValueError("Mode matrix and its derivative should be the same shape.")
+    mshapelist = list(mshape)
+    del mshapelist[axis]
+    
+    #Make Vphi, phidot and H into at least 1-d arrays
+    Vphi, phidot, H = np.atleast_1d(Vphi, phidot, H)
+    
+    #If Vphi doesn't have k axis then add it
+    if len(Vphi.shape) < len(phidot.shape):
+        Vphi = np.expand_dims(Vphi, axis=-1) 
+    
+    if len(mshapelist) != len(Vphi.shape) != len(phidot.shape):
+        raise ValueError("Vphi, phidot and modes arrays must have correct shape.")
+    
+    #If H doesn't have a field axis then add one
+    if len(H.shape) < len(phidot.shape):
+        H = np.expand_dims(H, axis)
+    
+    return Vphi, phidot, H, modes, modesdot, axis
+
 def deltarhosmatrix(Vphi, phidot, H, modes, modesdot, axis):
     """Matrix of the first order perturbed energy densities of the field components.
     
@@ -96,28 +155,13 @@ def deltarhosmatrix(Vphi, phidot, H, modes, modesdot, axis):
           (100,3,3,10) with nfields=3, then axis=1. The two mode matrix axes are
           assumed to be beside each other so (100,3,10,3) would not be valid.
     
+    Returns
+    -------
+    result: array_like
+            The matrix of the first order perturbed energy densities.
+    
     """
-    mshape = modes.shape
-    if mshape[axis+1] != mshape[axis]:
-        raise ValueError("The mode matrix dimensions are not together.")
-    if mshape != modesdot.shape:
-        raise ValueError("Mode matrix and its derivative should be the same shape.")
-    mshapelist = list(mshape)
-    del mshapelist[axis]
-    
-    #Make Vphi, phidot and H into at least 1-d arrays
-    Vphi, phidot, H = np.atleast_1d(Vphi, phidot, H)
-    
-    #If Vphi doesn't have k axis then add it
-    if len(Vphi.shape) < len(phidot.shape):
-        Vphi = np.expand_dims(Vphi, axis=-1) 
-    
-    if len(mshapelist) != len(Vphi.shape) != len(phidot.shape):
-        raise ValueError("Vphi, phidot and modes arrays must have correct shape.")
-    
-    #If H doesn't have a field axis then add one
-    if len(H.shape) < len(phidot.shape):
-        H = np.expand_dims(H, axis)
+    Vphi, phidot, H, modes, modesdot, axis = correct_shapes(Vphi, phidot, H, modes, modesdot, axis)
     
     #Change shape of phidot, Vphi, H to add extra dimension of modes
     Vphi = np.expand_dims(Vphi, axis+1)
@@ -134,10 +178,6 @@ def deltarhosmatrix(Vphi, phidot, H, modes, modesdot, axis):
     result += Vphi*modes
     
     return result
-    
-    
-    
-    
 
 def deltaprelmodes(Vphi, phidot, H, modes, modesdot, axis):
     """Perturbed relative pressure of the fields given as quantum mode functions.
@@ -168,27 +208,7 @@ def deltaprelmodes(Vphi, phidot, H, modes, modesdot, axis):
     
     """
     
-    mshape = modes.shape
-    if mshape[axis+1] != mshape[axis]:
-        raise ValueError("The mode matrix dimensions are not together.")
-    if mshape != modesdot.shape:
-        raise ValueError("Mode matrix and its derivative should be the same shape.")
-    mshapelist = list(mshape)
-    del mshapelist[axis]
-    
-    #Make Vphi, phidot and H into at least 1-d arrays
-    Vphi, phidot, H = np.atleast_1d(Vphi, phidot, H)
-    
-    #If Vphi doesn't have k axis then add it
-    if len(Vphi.shape) < len(phidot.shape):
-        Vphi = np.expand_dims(Vphi, axis=-1) 
-    
-    if len(mshapelist) != len(Vphi.shape) != len(phidot.shape):
-        raise ValueError("Vphi, phidot and modes arrays must have correct shape.")
-    
-    #If H doesn't have a field axis then add one
-    if len(H.shape) < len(phidot.shape):
-        H = np.expand_dims(H, axis)
+    Vphi, phidot, H, modes, modesdot, axis = correct_shapes(Vphi, phidot, H, modes, modesdot, axis)
     
     cs = soundspeeds(Vphi, phidot, H)
     rdots = rhodots(phidot, H)
@@ -220,6 +240,68 @@ def deltaprelspectrum(Vphi, phidot, H, modes, modesdot, axis):
     
     return spectrum
 
+def components_from_model(m, tix=None, kix=None):
+    """Get the component variables of delta Prel from a model instance.
+    
+    Arguments
+    ---------
+    m: Cosmomodels model instance
+       The model instance with which to perform the calculation
+       
+    tix: integer
+         Index for timestep at which to perform calculation. Default is to 
+         calculate over all timesteps.
+        
+    kix: integer
+         Index for k mode for which to perform the calculation. Default is to
+         calculate over all k modes.
+         
+    Returns
+    -------
+    Tuple containing:
+    
+    Vphi: array_like
+          First derivative of the potential with respect to the fields
+          
+    phidot: array_like
+            First derivative of the field values with respect to efold number N.
+            
+    H: array_like
+       The Hubble parameter
+       
+    modes: array_like
+           Mode matrix of first order perturbations. Component array should
+           have two dimensions of length nfields.
+    
+    modesdot: array_like
+           Mode matrix of N-derivative of first order perturbations. Component array should
+           have two dimensions of length nfields.
+    
+    axis: integer
+          Specifies which axis is first in mode matrix, e.g. if modes has shape
+          (100,3,3,10) with nfields=3, then axis=1. The two mode matrix axes are
+          assumed to be beside each other so (100,3,10,3) would not be valid.
+    """
+    if tix is None:
+        tslice = slice(None)
+    else:
+        tslice = slice(tix, tix+1)
+    if kix is None:
+        kslice = slice(None)
+    else:
+        kslice = slice(kix, kix+1)
+    
+    phidot = m.yresult[tslice, m.phidots_ix, kslice]
+    H = m.yresult[tslice, m.H_ix, kslice]
+    Vphi = np.array([m.potentials(myr, m.pot_params)[1] 
+                     for myr in m.yresult[tslice,m.bg_ix,kslice]])
+    modes = m.getmodematrix(m.yresult[tslice, m.dps_ix, kslice])
+    modesdot = m.getmodematrix(m.yresult[tslice, m.dpdots_ix, kslice])
+    axis = 1
+    Vphi, phidot, H, modes, modesdot, axis = correct_shapes(Vphi, phidot, H, modes, modesdot, axis)
+    
+    return Vphi,phidot,H,modes,modesdot,axis
+
 def dprel_from_model(m, tix=None, kix=None):
     """Get the spectrum of delta Prel from a model instance.
     
@@ -242,22 +324,7 @@ def dprel_from_model(m, tix=None, kix=None):
               Array of values of the power spectrum of the relativistic pressure
               perturbation
     """
-    if tix is None:
-        tslice = slice(None)
-    else:
-        tslice = slice(tix, tix+1)
-    if kix is None:
-        kslice = slice(None)
-    else:
-        kslice = slice(kix, kix+1)
-    
-    phidot = m.yresult[tslice, m.phidots_ix, kslice]
-    H = m.yresult[tslice, m.H_ix, kslice]
-    Vphi = np.array([m.potentials(myr, m.pot_params)[1] 
-                     for myr in m.yresult[tslice,m.bg_ix,kslice]])
-    modes = m.getmodematrix(m.yresult[tslice, m.dps_ix, kslice])
-    modesdot = m.getmodematrix(m.yresult[tslice, m.dpdots_ix, kslice])
-    axis = 1
-    result = deltaprelspectrum(Vphi, phidot, H, modes, modesdot, axis)
+    components = components_from_model(m, tix, kix)
+    result = deltaprelspectrum(*components)
     
     return result
