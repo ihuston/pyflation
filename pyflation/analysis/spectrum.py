912e03f993e212bebd152a728c6ce7316bbb2afc
Ian Huston <ian@ianhuston.net>
2011-10-10 18:44:08 +0100

    Start changing structure of functions and using new argument lists.

--- pyflation/analysis/spectrum.py
+++ pyflation/analysis/spectrum.py
@@ -3,7 +3,10 @@
 Author: ith
 '''
 
-def getmodematrix(self, y, ix=None, ixslice=None):
+import numpy as np
+from scipy import interpolate
+
+def getmodematrix(y, nfields, ix=None, ixslice=None):
     """Helper function to reshape flat nfield^2 long y variable into nfield*nfield mode
     matrix. Returns a view of the y array (changes will be reflected in underlying array).
     
@@ -25,65 +28,46 @@ def getmodematrix(self, y, ix=None, ixslice=None):
         ixslice = slice(None)
     indices = [Ellipsis]*len(y.shape)
     indices[ix] = ixslice
-    modes = y[indices]
+    modes = y[tuple(indices)]
         
     s = list(modes.shape)
     #Check resulting array is correct shape
-    if s[ix] != self.nfields**2:
-        raise ModelError("Array does not have correct dimensions of nfields**2.")
-    s[ix] = self.nfields
-    s.insert(ix+1, self.nfields)
+    if s[ix] != nfields**2:
+        raise ValueError("Array does not have correct dimensions of nfields**2.")
+    s[ix] = nfields
+    s.insert(ix+1, nfields)
     result = modes.reshape(s)
     return result
 
-def flattenmodematrix(self, modematrix, ix1=None, ix2=None):
+def flattenmodematrix(modematrix, nfields, ix1=None, ix2=None):
     """Flatten the mode matrix given into nfield^2 long vector."""
     s = modematrix.shape
-    if s.count(self.nfields) < 2:
-        raise ModelError("Mode matrix does not have two nfield long dimensions.")
+    if s.count(nfields) < 2:
+        raise ValueError("Mode matrix does not have two nfield long dimensions.")
     try:
         #If indices are not specified, use first two in order
         if ix1 is None:
-            ix1 = s.index(self.nfields)
+            ix1 = s.index(nfields)
         if ix2 is None:
             #The second index is assumed to be after ix1
-            ix2 = s.index(self.nfields, ix1+1)
+            ix2 = s.index(nfields, ix1+1)
     except ValueError:
-        raise ModelError("Cannot determine correct indices for nfield long dimensions!")
+        raise ValueError("Cannot determine correct indices for nfield long dimensions!")
     slist = list(s)
     ix2out = slist.pop(ix2)
-    slist[ix1] = self.nfields**2
+    slist[ix1] = nfields**2
     return modematrix.reshape(slist) 
     
     
-def deltaphi(self, recompute=False):
-    """Return the calculated values of $\delta\phi$ for all times, fields and modes.
-    
-    The result is stored as the instance variable self.deltaphi but will be recomputed
-    if `recompute` is True.
-    
-    Parameters
-    ----------
-    recompute: boolean, optional
-               Should the values be recomputed? Default is False.
-               
-    Returns
-    -------
-    deltaphi: array_like, dtype: complex128
-              Array of $\delta\phi$ values for all timesteps, fields and k modes.
-    """
-    
-    if not hasattr(self, "_deltaphi") or recompute:
-        self._deltaphi = self.yresult[:,self.dps_ix,:]
-    return self._deltaphi
 
 
-def Pphi(self, recompute=False):
+
+def Pphi(m, recompute=False):
     """Return the spectrum of scalar perturbations P_phi for each field and k.
     
     This is the unscaled version $P_{\phi}$ which is related to the scaled version by
     $\mathcal{P}_{\phi} = k^3/(2pi^2) P_{\phi}$. Note that result is stored as the
-    instance variable self.Pphi.
+    instance variable m.Pphi.
     For multifield systems the full crossterm matrix is returned which 
     has shape nfields*nfields flattened down to a vector of length nfields^2. 
     
@@ -97,36 +81,52 @@ def Pphi(self, recompute=False):
     Pphi: array_like, dtype: float64
           3-d array of Pphi values for all timesteps, fields and k modes
     """
-    #Basic caching of result
-    if not hasattr(self, "_Pphi") or recompute:     
-        #Get into mode matrix form, over first axis   
-        mdp = self.getmodematrix(self.yresult, 1, self.dps_ix)
-        #Take tensor product of modes and conjugate, summing over second mode
-        #index.
-        mPphi = np.zeros_like(mdp)
-        #Do for loop as tensordot to memory expensive
-        nfields=self.nfields
-        for i in range(nfields):
-            for j in range(nfields):
-                for k in range(nfields):
-                    mPphi[:,i,j] += mdp[:,i,k]*mdp[:,j,k].conj() 
-        #Flatten back into vector form
-        self._Pphi = self.flattenmodematrix(mPphi, 1, 2) 
-    return self._Pphi
+         
+    #Get into mode matrix form, over first axis   
+    mdp = getmodematrix(m.yresult, m.nfields, ix=1, ixslice=m.dps_ix)
+    #Take tensor product of modes and conjugate, summing over second mode
+    #index.
+    mPphi = np.zeros_like(mdp)
+    #Do for loop as tensordot too memory expensive
+    nfields=m.nfields
+    for i in range(nfields):
+        for j in range(nfields):
+            for k in range(nfields):
+                mPphi[:,i,j] += mdp[:,i,k]*mdp[:,j,k].conj() 
+    #Flatten back into vector form
+    Pphi = flattenmodematrix(mPphi, m.nfields, 1, 2) 
+    return Pphi
 
 
-def findns(self, k=None, nefolds=3):
-    """Return the value of n_s at the specified k mode, nefolds after horizon crossing."""
+def findns(m, k=None, nix=-1):
+    """Return the value of n_s
     
-    #If k is not defined, get value at all self.k
+    Arguments
+    ---------
+    m: Cosmomodels instance
+       Model for which to calculate n_s
+       
+    k: array_like, optional
+       k values at which to calculate n_s (should be inside range of ks used in model)
+       Defaults to all k values in m.
+       
+    nix: integer, optional
+         Timestep at which to calculate n_s, defaults to last timestep
+         
+    Returns
+    -------
+    n_s: float
+         The value of the spectral index at the requested k value and timestep
+    """
+    
+    #If k is not defined, get value at all m.k
     if k is None:
-        k = self.k
+        k = m.k
     else:
-        if k<self.k.min() and k>self.k.max():
-            self._log.warn("Warning: Extrapolating to k value outside those used in spline!")
+        if np.any(k<m.k.min() or k>m.k.max()):
+            m._log.warn("Warning: Extrapolating to k value outside those used in spline!")
     
-    ts = self.findallkcrossings(self.tresult, self.yresult[:,2], factor=1)[:,0] + nefolds/self.tstep_wanted #About nefolds after horizon exit
-    xp = np.log(self.Pr[ts.astype(int)].diagonal())
+    xp = np.log(m.Pr[nix])
     lnk = np.log(k)
     
     #Need to sort into ascending k
@@ -143,12 +143,12 @@ def findns(self, k=None, nefolds=3):
     
     return nsunsort
 
-def findHorizoncrossings(self, factor=1):
+def findHorizoncrossings(m, factor=1):
     """Find horizon crossing for all ks"""
-    return self.findallkcrossings(self.tresult, self.yresult[:,2], factor)
+    return m.findallkcrossings(m.tresult, m.yresult[:,2], factor)
      
      
-def Pr(self, recompute=False):
+def Pr(m):
     """Return the spectrum of curvature perturbations $P_R$ for each k.
     
     For a multifield model this is given by:
@@ -161,7 +161,7 @@ def Pr(self, recompute=False):
     
     This is the unscaled version $P_R$ which is related to the scaled version by
     $\mathcal{P}_R = k^3/(2pi^2) P_R$. Note that result is stored as the instance variable
-    self.Pr. 
+    m.Pr. 
     
     Parameters
     ----------
@@ -172,23 +172,21 @@ def Pr(self, recompute=False):
     -------
     Pr: array_like, dtype: float64
         Array of Pr values for all timesteps and k modes
-    """
-    #Basic caching of result
-    if not hasattr(self, "_Pr") or recompute:        
-        phidot = np.float64(self.yresult[:,self.phidots_ix,:]) #bg phidot
-        phidotsumsq = (np.sum(phidot**2, axis=1))**2
-        #Get mode matrix for Pphi as nfield*nfield
-        Pphimatrix = self.getmodematrix(self.Pphi, 1, slice(None))
-        #Multiply mode matrix by corresponding phidot value
-        summatrix = phidot[:,np.newaxis,:,:]*phidot[:,:,np.newaxis,:]*Pphimatrix
-        #Flatten mode matrix and sum over all nfield**2 values
-        sumflat = np.sum(self.flattenmodematrix(summatrix, 1, 2), axis=1)
-        #Divide by total sum of derivative terms
-        self._Pr = sumflat/phidotsumsq
-    return self._Pr
+    """      
+    phidot = np.float64(m.yresult[:,m.phidots_ix,:]) #bg phidot
+    phidotsumsq = (np.sum(phidot**2, axis=1))**2
+    #Get mode matrix for Pphi as nfield*nfield
+    Pphimatrix = getmodematrix(Pphi(m), m.nfields, 1, slice(None))
+    #Multiply mode matrix by corresponding phidot value
+    summatrix = phidot[:,np.newaxis,:,:]*phidot[:,:,np.newaxis,:]*Pphimatrix
+    #Flatten mode matrix and sum over all nfield**2 values
+    sumflat = np.sum(flattenmodematrix(summatrix, m.nfields, 1, 2), axis=1)
+    #Divide by total sum of derivative terms
+    Pr = sumflat/phidotsumsq
+    return Pr
 
 
-def calPr(self):
+def calPr(m):
     """Return the spectrum of curvature perturbations $\mathcal{P}_\mathcal{R}$ 
     for each timestep and k mode.
     
@@ -201,4 +199,4 @@ def calPr(self):
         Array of Pr values for all timesteps and k modes
     """
     #Basic caching of result
-    return 1/(2*np.pi**2) * self.k**3 * self.Pr           
+    return 1/(2*np.pi**2) * m.k**3 * Pr(m)           
