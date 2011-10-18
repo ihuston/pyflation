b39dea982c03ae0da58f8d98c974c3925b59a9f1
Ian Huston <ian@ianhuston.net>
2011-10-18 15:18:57 +0100

    Added tests for total sound speed.

--- pyflation/analysis/test/test_deltaprel.py
+++ pyflation/analysis/test/test_deltaprel.py
@@ -5,6 +5,7 @@ For license and copyright information see LICENSE.txt which was distributed with
 '''
 import numpy as np
 from numpy.testing import assert_, assert_raises, \
+                          assert_equal,\
                           assert_array_almost_equal, \
                           assert_almost_equal
 from pyflation.analysis import deltaprel
@@ -49,6 +50,53 @@ class TestSoundSpeeds():
         H = np.array([[2],[1]]).reshape((2,1,1))
         arr = deltaprel.soundspeeds(Vphi, phidot, H)
         actual = np.array([[31/30.0,4.0/3], [9.0/7,3]]).reshape((2,2,1))
+        assert_array_almost_equal(arr, actual)
+        
+class TestTotalSoundSpeed():
+    
+    def setup(self):
+        self.Vphi = np.arange(24).reshape((4,3,2))
+        self.phidot = self.Vphi
+        self.H = np.arange(8).reshape((4,1,2))
+        self.axis = 1
+    
+
+    def test_shape(self):
+        """Test whether the soundspeeds are shaped correctly."""    
+        arr = deltaprel.totalsoundspeed(self.Vphi, self.phidot, self.H, self.axis)
+        result = arr.shape
+        newshape = list(self.phidot.shape)
+        del newshape[self.axis]
+        actual = tuple(newshape)
+        assert_(result == actual, "Result shape %s, but desired shape is %s"%(str(result), str(actual)))
+        
+    def test_scalar(self):
+        """Test results of 1x1x1 calculation."""
+        arr = deltaprel.totalsoundspeed(3, 0.5, 2, 0)
+        assert_equal(arr, 2)
+        
+    def test_two_by_one_by_one(self):
+        """Test results of 2x1x1 calculation."""
+        Vphi = np.array([5,10]).reshape((2,1,1))
+        phidot = np.array([3,6]).reshape((2,1,1))
+        H = np.array([1,2]).reshape((2,1,1))
+        arr = deltaprel.totalsoundspeed(Vphi, phidot, H, axis=1)
+        actual = np.array([19/9.0, 92/72.0]).reshape((2,1,1))
+        assert_array_almost_equal(arr, actual)
+        
+    def test_wrongshape(self):
+        """Test that wrong shapes raise exception."""
+        self.H = np.arange(8).reshape((4,2))
+        assert_raises(ValueError, deltaprel.totalsoundspeed, self.Vphi, self.phidot, self.H, self.axis)
+        
+    def test_two_by_two_by_one(self):
+        """Test results of 2x2x1 calculation."""
+        Vphi = np.array([[1,2],[3,9]]).reshape((2,2,1))
+        phidot = np.array([[5,1],[7,3]]).reshape((2,2,1))
+        H = np.array([[2],[1]]).reshape((2,1,1))
+        axis = 1
+        arr = deltaprel.totalsoundspeed(Vphi, phidot, H, axis)
+        actual = np.array([163/156.0,135.0/87.0]).reshape((2,1))
         assert_array_almost_equal(arr, actual)
 
 class TestPdots():
