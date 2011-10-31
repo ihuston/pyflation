69ed821dd833a751befd14109c21ba3a8e1f9007
Ian Huston <ian@ianhuston.net>
2011-10-31 14:25:32 +0000

    Change init to import some functions.

--- pyflation/analysis/__init__.py
+++ pyflation/analysis/__init__.py
@@ -3,3 +3,6 @@
 Author: Ian Huston
 For license and copyright information see LICENSE.txt which was distributed with this file.
 """
+
+from adiabatic import Pr, Pzeta, scaledPr
+from nonadiabatic import deltaPspectrum, deltapnadspectrum, deltarhospectrum
