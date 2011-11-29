259c89cb0d2223ad1cab817ef0b7958c74e5fb20
Ian Huston <ian@ianhuston.net>
2011-11-29 12:09:57 +0000

    Change requires arguments to be compatible with distutils.

--- setup.py
+++ setup.py
@@ -58,12 +58,12 @@ cosmological perturbations during an inflationary expansion of the universe.""",
                                "Topic :: Scientific/Engineering :: Astronomy",
                                "Topic :: Scientific/Engineering :: Physics"
                                ],
-                  install_requires=["numpy >= 1.3",
+                  requires=["numpy >= 1.3",
                                     "scipy >= 0.7.1",
                                     "Cython >= 0.12.1",
                                     "tables >= 2.2"],
                   #Include dependency on matplotlib for cosmographs
-                  extras_require=["matplotlib >= 1.0.1"]
+                  #extras_require=["matplotlib >= 1.0.1"]
                   )
 
 if __name__ == "__main__":
