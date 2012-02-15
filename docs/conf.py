7ac3b240da25323a1152a79a76df740a5fe3a2d3
Ian Huston <ihuston@qmul.ac.uk>
2012-02-15 11:52:22 +0000

    Add logo to Latex docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
-              'sphinx.ext.autosummary', 'numpydoc']
+              'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -190,7 +190,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = "./_static/header.png"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
