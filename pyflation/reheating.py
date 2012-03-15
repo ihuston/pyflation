Remove erroneous extra factor of H.

pyflation/reheating.py
@@ -161,7 +161,7 @@ class ReheatingBackground(ReheatingModels):
         dydx[self.phis_ix] = phidots
         
         #dphi^prime/dn
-        dydx[self.phidots_ix] = -((3 + Hdot/H + 0.5/H**2 * (tgamma + tmatter))*phidots 
+        dydx[self.phidots_ix] = -((3 + Hdot/H + 0.5/H * (tgamma + tmatter))*phidots 
                                    + dUdphi[...,np.newaxis]/(H**2))
         
         #dH/dn