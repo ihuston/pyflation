b4bb26c363850b3db62a5cd38cf9cc5db8ea3131
Ian Huston <ian@ianhuston.net>
2012-10-05 12:03:01 +0100

    Fix step to be difference with starting x value.

--- pyflation/rk4.py
+++ pyflation/rk4.py
@@ -437,7 +437,7 @@ def rkdriver_rkf45(ystart, xstart, xend, h, derivs, yarr, xarr,
         #Check whether modes need to be initialised
         ks_starting = np.where((last_x + h > xstart) & (~has_started) )[0]
         if ks_starting:
-            h = np.min(xstart[ks_starting])
+            h = np.min(xstart[ks_starting]) - last_x
             rk_log.info("Step size changed to %f for initialization.", h)
             next_ix = np.argmin(xstart[ks_starting])
             #Refresh to latest h
