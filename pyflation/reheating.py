Fix minus sign changes having rederived equations.

pyflation/reheating.py
@@ -437,10 +437,10 @@ class ReheatingFirstOrder(ReheatingModels):
                                         + rhogamma*dydx[self.Vgamma_ix]))
             
             #Fluid perturbations
-            dydx[self.dgamma_ix] = -(4*dgamma + 4*k**2/(3*H*a**2)*Vgamma*rhogamma
-                                     -2/(3*H**2) * rhogamma*drho_full - 4*rhogamma*metric_phi)
-            dydx[self.dmatter_ix] = -(3*dmatter + k**2/(H*a**2)*Vmatter*rhomatter
-                                     -1/(2*H**2) * rhomatter*drho_full - 3*rhomatter*metric_phi)
+            dydx[self.dgamma_ix] = -(4*dgamma - 4*k**2/(3*H*a**2)*Vgamma*rhogamma
+                                     +2/(3*H**2) * rhogamma*drho_full + 4*rhogamma*metric_phi)
+            dydx[self.dmatter_ix] = -(3*dmatter - k**2/(H*a**2)*Vmatter*rhomatter
+                                     +1/(2*H**2) * rhomatter*drho_full + 3*rhomatter*metric_phi)
             
             #Field perturbations
             dydx[self.dps_ix] = 0
@@ -482,7 +482,7 @@ class ReheatingFirstOrder(ReheatingModels):
             drho_full = (dmatter + dgamma 
                          + np.sum(H**2*phidots[:,np.newaxis]*dpdotmodes 
                                   -H**2*phidots[:,np.newaxis]**2*metric_phi
-                                  -dUdphi[:,np.newaxis]*dpmodes, axis=0))
+                                  +dUdphi[:,np.newaxis]*dpmodes, axis=0))
             
             #Vmatter and Vgamma perturbation equations
             if np.any(active_transfers[:,self.tmatter_ix]*np.nan_to_num(rhomatter[0])):
@@ -511,13 +511,13 @@ class ReheatingFirstOrder(ReheatingModels):
                               )
             
             #Fluid perturbations
-            dydx[self.dgamma_ix] = (-4*dgamma - 4*k**2/(3*H*a**2)*Vgamma*rhogamma
-                                     +2/(3*H**2) * rhogamma*drho_full + 4*rhogamma*metric_phi
+            dydx[self.dgamma_ix] = (-4*dgamma + 4*k**2/(3*H*a**2)*Vgamma*rhogamma
+                                     -2/(3*H**2) * rhogamma*drho_full - 4*rhogamma*metric_phi
                                      +np.sum(H*tgamma*(phidots[:,np.newaxis]*dpdotmodes
                                         -0.5*phidots[:,np.newaxis]**2*metric_phi[np.newaxis,:]), axis=0))
             
-            dydx[self.dmatter_ix] = (-3*dmatter - k**2/(H*a**2)*Vmatter*rhomatter
-                                     +1/(2*H**2) * rhomatter*drho_full + 3*rhomatter*metric_phi
+            dydx[self.dmatter_ix] = (-3*dmatter + k**2/(H*a**2)*Vmatter*rhomatter
+                                     -1/(2*H**2) * rhomatter*drho_full - 3*rhomatter*metric_phi
                                      +np.sum(H*tmatter*(phidots[:,np.newaxis]*dpdotmodes
                                         -0.5*phidots[:,np.newaxis]**2*metric_phi[np.newaxis,:]), axis=0))
             #This for loop runs over i,j and does the inner summation over l
@@ -534,9 +534,9 @@ class ReheatingFirstOrder(ReheatingModels):
                                      + innerterm/H**2
                                      + (2/H**2 * dUdphi[:,np.newaxis] 
                                         + 0.5/H*phidots[:,np.newaxis]*(tgamma+tmatter)
-                                        - 3*phidots[:,np.newaxis])*metric_phi[np.newaxis,:]
+                                        + 3*phidots[:,np.newaxis])*metric_phi[np.newaxis,:]
                                      - phidots[:,np.newaxis]*metric_phi_dot[np.newaxis,:]
-                                     - 1/(2*H**2) * phidots[:,np.newaxis]*drho_full[np.newaxis,:]
+                                     + 1/(2*H**2) * phidots[:,np.newaxis]*drho_full[np.newaxis,:]
                                      )
         return dydx
     